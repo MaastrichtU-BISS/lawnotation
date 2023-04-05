c55479d44e0e96ed1b901885486de5bf7cb80da0
Carlos <carloseam94@gmail.com>
2023-04-05 13:58:09 +0200

    add: supabase support and config

--- lawnotation-ui/app.vue
+++ lawnotation-ui/app.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h3>Lawnotation</h3>
   </div>
 </template>
+<script setup lang="ts">
+import { createClient } from '@supabase/supabase-js'
+const config = useRuntimeConfig();
+
+onMounted(() => {
+  const supabase = createClient(config.apiUrl, config.apiAnonKey)
+  console.log(supabase)
+})
+
+</script>
--- lawnotation-ui/nuxt.config.ts
+++ lawnotation-ui/nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    runtimeConfig: {
+        public: {
+          apiUrl: process.env.SUPABASE_LAWNOTATION_URL || "",
+          apiAnonKey: process.env.SUPABASE_LAWNOTATION_ANON_KEY || ""
+        }
+      }
 })
