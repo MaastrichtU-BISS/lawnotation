443481feb3d41f864f3186465fc21b87cb6db5f1
Carlos <carloseam94@gmail.com>
2023-04-17 10:09:37 +0200

    add: task creation inside a project

---
+++ lawnotation-ui/data/task.ts
@@ -0,0 +1,63 @@
+
+export type Task = {
+    id: number,
+    name: string,
+    desc: string,
+    project_id: number,
+    label_id: number
+  }
+  
+  export const useTaskApi = () => {
+    const supabase = useSupabaseClient();
+    
+    // Create
+    const createTask = async (fields: Omit<Task, 'id'>): Promise<Task> => {
+      const { data, error } = await supabase.from("tasks").insert(fields).select().single();
+      if (error)
+        throw Error(`Error in createTask: ${error.message}`)
+      else
+        return data as Task;
+    };
+  
+    // Read
+    const findTask = async (id: string): Promise<Task>   => {
+      const { data, error } = await supabase.from("tasks").select().eq("id", id).single();
+  
+      if (error)
+        throw Error(`Error in findTask: ${error.message}`)
+      else
+        return data as Task
+    };
+  
+    // Read all
+    const findTasks = async (project_id: string): Promise<Task[]> => {
+      const { data, error } = await supabase.from("tasks").select().eq("project_id", project_id);
+      
+      if (error)
+        throw Error(`Error in findTask: ${error.message}`)
+      else
+        return data as Task[]
+    };
+  
+    // Update
+    const updateTask = async (id: string, fields: Partial<Task>): Promise<boolean> => {
+      const { data, error } = await supabase.from("tasks").update(fields).eq("id", id);
+      
+      if (error)
+        throw Error(`Error in updateTask: ${error.message}`)
+      else
+        return true;
+    };
+  
+    // Update
+    const deleteTask = async (id: string) => {
+      const { data, error } = await supabase.from("tasks").delete().eq("id", id);
+  
+      if (error)
+        throw Error(`Error in deleteTask: ${error.message}`)
+      else
+        return true;
+    };
+  
+    return {createTask, findTask, findTasks, updateTask, deleteTask}
+  }
--- lawnotation-ui/pages/projects/[project_id]/index.vue
+++ lawnotation-ui/pages/projects/[project_id]/index.vue
@@ -3,7 +3,7 @@
     <h3 class="my-2 text-lg font-semibold mb-2">Project</h3>
     <div>{{ project }}</div>
     <h3 class="my-2">Documents: {{ documents.length }}</h3>
-    <ul v-for="d in documents" :key="d.id" class="list-disc list-inside">
+    <ul v-for="d in documents" :key="'doc_' + d.id" class="list-disc list-inside">
       <li>
         <span
           >{{ d.id }}.
@@ -24,19 +24,48 @@
         @change="change_file($event)"
       />
     </div>
-    <span>Go to: </span><NuxtLink :to="`/projects/${project.id}/tasks`">Tasks</NuxtLink>
+    <h3 class="my-2">Tasks: {{ tasks.length }}</h3>
+    <ul v-for="t in tasks" :key="'task_' + t.id" class="list-disc list-inside">
+      <li>
+        <span
+          >{{ t.id }}.
+          <NuxtLink :to="`/projects/${route.params.project_id}/tasks/${t.id}`">{{
+            t.name
+          }}</NuxtLink></span
+        >
+      </li>
+    </ul>
+    <div class="my-3">
+      <div class="flex flex-col w-1/2 space-y-2 border-t border-neutral-300 mt-3 pt-3">
+        <input type="text" placeholder="Project name" v-model="new_task.name" />
+        <textarea placeholder="Project description" v-model="new_task.desc"></textarea>
+        <label for="label_id">Labels Id</label>
+        <input type="number" name="" id="label_id" v-model="new_task.label_id" />
+        <button class="btn-primary" @click="addTask">Add Tasks</button>
+      </div>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
 import { Project, useProjectApi } from "~/data/project";
 import { Document, useDocumentApi } from "~/data/document";
-const supabase = useSupabaseClient();
+import { Task, useTaskApi } from "~/data/task";
+
 const projectApi = useProjectApi();
 const documentApi = useDocumentApi();
+const taskApi = useTaskApi();
 
 const route = useRoute();
 const project = ref<Project>();
 const documents = reactive<Document[]>([]);
+const tasks = reactive<Task[]>([]);
+
+const new_task = reactive<Task>({
+  name: "",
+  desc: "",
+  label_id: 1,
+  project_id: 0,
+});
 
 const change_file = (event: Event) => {
   Array.from(event.target.files).forEach((file: File) => {
@@ -57,11 +86,30 @@ const change_file = (event: Event) => {
   });
 };
 
+const addTask = () => {
+  if (new_task.name == "") {
+    alert("name required");
+    return;
+  }
+  if (new_task.desc == "") {
+    alert("desc required");
+    return;
+  }
+
+  taskApi.createTask(new_task).then((task) => {
+    tasks.push(task);
+  });
+};
+
 onMounted(() => {
   projectApi.findProject(route.params.project_id.toString()).then((p) => {
     project.value = p;
+    new_task.project_id = p.id;
     documentApi.findDocuments(p.id.toString()).then((docs) => {
       documents.splice(0) && documents.push(...docs);
+    });
+    taskApi.findTasks(p.id.toString()).then((_tasks) => {
+      tasks.splice(0) && tasks.push(..._tasks);
     });
   });
 });
