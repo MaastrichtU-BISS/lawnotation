9ab3a6beed19433e4565b3cc8ebe11130bce68d8
eensander <50321538+eensander@users.noreply.github.com>
2023-04-18 17:41:27 +0200

    add begin labelset

---
+++ lawnotation-ui/pages/labelset/[labelset_id]/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-if="labelset">
+      Wowie
+  </div>
+</template>
+<script setup lang="ts">
+import { Labelset, useLabelsetApi } from "~/data/labelset";
+const route = useRoute();
+
+const user = useSupabaseUser();
+const labelsetApi = useLabelsetApi();
+
+const labelset = ref<Labelset>();
+
+onMounted(() => {
+  labelsetApi.findLabelset(route.params.labelset_id.toString()).then((_labelset) => {
+    labelset.value = _labelset;
+  });
+});
+
+definePageMeta({
+  middleware: ["auth"],
+});
+</script>
