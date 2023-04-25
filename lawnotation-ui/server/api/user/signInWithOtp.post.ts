6fca35331aa86ee2a552bd8a8557beef4d6e0fd9
eensander <50321538+eensander@users.noreply.github.com>
2023-04-25 10:04:12 +0200

    fix: sign in otp with anon key instead of service key

--- lawnotation-ui/server/api/user/signInWithOtp.post.ts
+++ lawnotation-ui/server/api/user/signInWithOtp.post.ts
@@ -1,6 +1,6 @@
-import { serverSupabaseServiceRole } from '#supabase/server'
+import { serverSupabaseClient } from '#supabase/server'
 export default eventHandler(async (event) => {
-  const client = serverSupabaseServiceRole(event)
+  const client = serverSupabaseClient(event)
   const body = await readBody(event)
   await client.auth.signInWithOtp({email: body.email, options: { emailRedirectTo: body.redirectTo } })
   const { data } = await client.from("users").select().eq("email", body.email).single();
