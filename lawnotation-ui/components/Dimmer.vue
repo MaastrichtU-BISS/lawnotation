b91b5de954161928e558dac2dc9e6c8804947e8d
Carlos <carloseam94@gmail.com>
2023-05-01 15:07:27 +0200

    add: dimmer applied to login page

---
+++ lawnotation-ui/components/Dimmer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div v-if="modelValue" class="dimmer">
+    <half-circle-spinner
+      v-if="modelValue"
+      :animation-duration="1000"
+      :size="60"
+      color="rgb(79 70 229)"
+      style="position: absolute; left: calc(50% - 30px); top: calc(50% - 30px)"
+    />
+  </div>
+</template>
+<script setup lang="ts">
+import { HalfCircleSpinner } from "epic-spinners";
+const props = defineProps<{
+  modelValue: boolean;
+}>();
+</script>
--- lawnotation-ui/pages/auth/login/index.vue
+++ lawnotation-ui/pages/auth/login/index.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="my-2 mx-auto max-w-lg text-center">
-    <div>
-      <div class="my-2">
-        <label for="email" class="pr-2">email</label>
-        <input
-          v-model="email"
-          type="email"
-          name="email"
-          autocomplete="off"
-          class="border"
-        />
-      </div>
-      <div class="my-2">
-        <button class="btn-primary rounded-full" @click="signIn()">Log In</button>
+    <div class="dimmer-wrapper">
+      <Dimmer v-model="loading" />
+      <div class="dimmer-content p-4">
+        <div class="my-2">
+          <label for="email" class="pr-2">email</label>
+          <input
+            v-model="email"
+            type="email"
+            name="email"
+            autocomplete="off"
+            class="border"
+          />
+        </div>
+        <div class="my-2">
+          <button class="btn-primary rounded-full" @click="signIn()">Log In</button>
+        </div>
       </div>
     </div>
   </div>
@@ -25,13 +28,16 @@ const toast = useToast();
 
 const userApi = useUserApi();
 const config = useRuntimeConfig();
+const loading = ref(false);
 
 const email = ref<string>("");
 
 const signIn = () => {
+  loading.value = true;
   try {
     if (email.value.length == 0) {
       toast.error("Enter your email");
+      loading.value = false;
       return;
     }
 
@@ -39,13 +45,16 @@ const signIn = () => {
       .otpLogin(email.value, `${config.public.baseURL}/projects`)
       .then((user) => {
         toast.success(`Login link has been sent to: ${email.value}`);
+        loading.value = false;
       })
       .catch((error) => {
         toast.error(`Error logging in with OTP: ${error.message}`);
+        loading.value = false;
       });
   } catch (error) {
     if (error instanceof Error)
       toast.error(`Error logging in with OTP: ${error.message}`);
+    loading.value = false;
   }
 };
 </script>
