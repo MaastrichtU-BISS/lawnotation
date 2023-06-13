af8ecb5d6276b9c96fcfbb4ff611be2db55394ad
eensander <50321538+eensander@users.noreply.github.com>
2023-06-13 17:14:10 +0200

    change: colors of breadcrumb

--- lawnotation-ui/components/Breadcrumb.vue
+++ lawnotation-ui/components/Breadcrumb.vue
@@ -4,23 +4,23 @@
       <li class="inline-flex items-center" v-for="(crumb, i) of props.crumbs" :key="crumb.link">
         <svg v-if="i > 0" aria-hidden="true" class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
 
-        <NuxtLink v-if="i < props.crumbs.length-1" :to="crumb.link" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+        <NuxtLink v-if="i < props.crumbs.length-1" :to="crumb.link" class="inline-flex items-center text-sm font-medium text-gray-500 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
           {{ crumb.name }}
         </NuxtLink>
-        <span v-else class="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">
+        <span v-else class="ml-1 text-sm font-medium text-gray-700 select-none md:ml-2">
           {{ crumb.name }}
         </span>
       </li>
       <li v-if="false">
         <div class="flex items-center">
           <svg aria-hidden="true" class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-          <a href="#" class="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white">Projects</a>
+          <a href="#" class="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:hover:text-white">Projects</a>
         </div>
       </li>
       <li v-if="false">
         <div class="flex items-center">
           <svg aria-hidden="true" class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-          <span class="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">Flowbite</span>
+          <span class="ml-1 text-sm font-medium text-gray-500 md:ml-2">Flowbite</span>
         </div>
       </li>
     </ol>
