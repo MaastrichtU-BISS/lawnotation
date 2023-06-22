784d8b093972c04b40a36a84d0b147fd12c36fa2
eensander <50321538+eensander@users.noreply.github.com>
2023-06-22 16:52:14 +0200

    add: concept server controllers for CRUD operations on relation entity (and C for user)

---
+++ lawnotation-ui/server/api/relation/index.get.ts
@@ -0,0 +1,3 @@
+export default eventHandler(async (event) => {
+  return "Server handler for retrieving of multiple relation entities";
+})
---
+++ lawnotation-ui/server/api/relation/index.post.ts
@@ -0,0 +1,3 @@
+export default eventHandler(async (event) => {
+  return "Server handler for creation of relation entity";
+})
