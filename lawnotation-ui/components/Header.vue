9c11bf3fa4459e1bdaf355692fb7157457f6e25f
eensander <50321538+eensander@users.noreply.github.com>
2023-07-14 15:47:44 +0200

    center header menu links and add style to active

--- lawnotation-ui/components/Header.vue
+++ lawnotation-ui/components/Header.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="bg-neutral-100 px-2 py-4 flex justify-between">
+  <div class="bg-neutral-100 px-4 py-4 flex justify-between items-center">
     <div class="w-40">
       <NuxtLink to="/"><img src="/lawnotation-logo.svg" /></NuxtLink>
     </div>
     <div v-if="user" class="space-x-4">
       <template v-if="role == 'editor'">
-        <NuxtLink to="/projects">Projects</NuxtLink>
-        <NuxtLink to="/labelset">Labelsets</NuxtLink>
+        <NuxtLink to="/projects" class="header-link" :class="{'active': routeIsActive('/projects')}">Projects</NuxtLink>
+        <NuxtLink to="/labelset" class="header-link" :class="{'active': routeIsActive('/labelset')}">Labelsets</NuxtLink>
         <span class="text-gray-400 select-none">|</span>
       </template>
-      <NuxtLink to="/tasks">Assigned Tasks</NuxtLink>
+      <NuxtLink to="/tasks" class="header-link" :class="{'active': routeIsActive('/tasks')}">Assigned Tasks</NuxtLink>
     </div>
     <div class="auth">
       <template v-if="user">
@@ -24,12 +24,20 @@
   </div>
 </template>
 <script setup lang="ts">
-import { useUserApi, User } from "../data/user";
+import { useUserApi, User } from "@/data/user";
 const supabase = useSupabaseClient();
 const user = useSupabaseUser();
 const userApi = useUserApi();
 
 const role = ref<string>();
+
+const route = useRoute();
+
+const routeIsActive = computed(() => {
+  return (match: string) => {
+    return route.path.startsWith(match);
+  }
+})
 
 onMounted(async () => {
   if (user.value) {
@@ -46,3 +54,18 @@ onMounted(async () => {
   }
 });
 </script>
+
+<style lang="scss">
+.header-link {
+  @apply text-gray-700 no-underline;
+  transition: color 0.2s;
+
+  &:hover {
+    @apply text-gray-900;
+  }
+
+  &.active {
+    @apply text-gray-900 border-b-2 border-slate-500;
+  }
+}
+</style>
