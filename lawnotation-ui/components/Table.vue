50381b5a05be6c149be4c11f1fc8d07c6489b155
eensander <50321538+eensander@users.noreply.github.com>
2023-07-17 15:19:47 +0200

    fix: table search input magnifying glass style

--- lawnotation-ui/components/Table.vue
+++ lawnotation-ui/components/Table.vue
@@ -15,7 +15,7 @@
           <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
             <svg class="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
           </div>
-          <input v-model="tabledata.search.query" type="text" class="block text-gray-900 bg-gray-50 p-2 !pl-10 text-sm w-80" :placeholder="`Search for ${tabledata.search.column}`">
+          <input v-model="tabledata.search.query" type="text" class="rounded-md border-0 bg-gray-50 p-2 pl-10 text-sm w-80 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400" :placeholder="`Search for ${tabledata.search.column}`">
         </div>
       </div>
     </div>
