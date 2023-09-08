9194fc423dd6bde83485f77b2695359fb1d485c8
eensander <50321538+eensander@users.noreply.github.com>
2023-09-08 16:47:16 +0200

    delegate types to dedicated folder

---
+++ lawnotation-ui/types/annotation.ts
@@ -0,0 +1,37 @@
+export type Annotation = {
+  id: number;
+  assignment_id: number;
+  start_index: number;
+  end_index: number;
+  text: string;
+  origin: string;
+  ls_id: string;
+  label: string;
+};
+
+export type RichAnnotation = {
+  start: number;
+  end: number;
+  text: string;
+  label: string;
+  annotator: string;
+  hidden: Boolean;
+  ann_id: number;
+  doc_id: string;
+};
+
+export type LSSerializedAnnotation = {
+  id: string;
+  from_name: string;
+  to_name: string;
+  origin: string;
+  type: string;
+  value: {
+    start: number;
+    end: number;
+    text: string;
+    labels: string[];
+  };
+};
+
+export type LSSerializedAnnotations = LSSerializedAnnotation[]; 
---
+++ lawnotation-ui/types/document.ts
@@ -0,0 +1,7 @@
+export type Document = {
+  id: number;
+  name: string;
+  project_id: string;
+  source: string;
+  full_text: string;
+};
