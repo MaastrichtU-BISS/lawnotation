d6528ed5287de85050e7ae7717d3319c3db17e2f
CvdL-UM <c.vanderlans@maastrichtuniversity.nl>
2023-09-12 14:26:14 +0200

    Added a first draft for the privacy policy and link to it from the login screen

---
+++ lawnotation-ui/pages/privacy-policy.vue
@@ -0,0 +1,73 @@
+<template>
+  <h1>Privacy Policy</h1>
+  <p>Effective Date: 2023-08-17</p>
+  <p>
+    Thank you for using Lawnotation (“we,” “our”, “us”, or “our platform”). To best
+    protect your privacy, we only collect your email address. This Privacy Policy explains
+    how we collect, use, disclose, and safeguard your email address when you access or use
+    our online platform. By accessing or using our platform, you consent to the terms of
+    this Privacy Policy.
+  </p>
+  <h2>Information We Collect</h2>
+  <p>
+    We collect your email address when you voluntarily provide it to us through our
+    platform by signing up for an account. We do not collect any other personally
+    identifiable information.
+  </p>
+  <h2>How We Use Your Email Address</h2>
+  <p>We use the email addresses we collect for the following purposes:</p>
+  <p>
+    <strong>Sending login emails:</strong> when you login on our platform using your email
+    address for the first time, we automatically create an account for you using your
+    email address and send you an email with a link to login on our platform. After we
+    have created an account using your email address, every time you login using your
+    email address we just send an email with a link to login on our platform.
+  </p>
+  <h2>Disclosure of Your Email Address</h2>
+  <p>
+    We do not sell, trade, or otherwise transfer your email address to outside parties. We
+    may, however, share your email address with a trusted third-party service provider to
+    help us with email delivery services. This third party is obligated to keep your
+    information confidential and secure.
+  </p>
+  <h2>Data Security</h2>
+  <p>
+    We use industry-standard security measures to protect your email address from
+    unauthorized access, alteration, disclosure, or destruction. However, no method of
+    transmission over the internet or electronic storage is completely secure, and we
+    cannot guarantee the absolute security of your information.
+  </p>
+  <h2>Your Rights</h2>
+  <p>
+    You have the right to access the personal information we hold about you and to request
+    that we correct, update, or delete it.
+  </p>
+  <h2>Changes to this Privacy Policy</h2>
+  <p>
+    We may update this Privacy Policy from time to time to reflect changes to our
+    practices or for other operational, legal, or regulatory reasons. We will notify you
+    of any material changes by posting the updated Privacy Policy on our platform. Your
+    continued use of the platform after such changes signifies your acceptance of the
+    updated Privacy Policy.
+  </p>
+  <h2>Contact Us</h2>
+  <p>
+    If you have any questions, concerns, or requests regarding this Privacy Policy or how
+    we handle your email address, please contact us at [contact email].
+  </p>
+</template>
+
+<style lang="postcss" scoped>
+h1 {
+  @apply font-bold text-4xl;
+}
+h2 {
+  @apply font-bold text-2xl;
+}
+p {
+  @apply mb-6 mt-2;
+}
+p + p {
+  @apply -mt-2;
+}
+</style>
