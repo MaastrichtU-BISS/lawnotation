561e1deb66b561c54fa9f9d9c18b878a92bff01b
CvdL-UM <c.vanderlans@maastrichtuniversity.nl>
2023-09-15 17:27:55 +0200

    Seed the local Supabase db

---
+++ lawnotation-ui/supabase/seed.sql
@@ -0,0 +1,9 @@
+-- Insert user test@test.com
+insert into auth.users (id, instance_id, aud, role, email, encrypted_password, email_confirmed_at, invited_at, confirmation_token, confirmation_sent_at, recovery_token, recovery_sent_at, email_change_token_new, email_change, email_change_sent_at, last_sign_in_at, raw_app_meta_data, raw_user_meta_data, is_super_admin, created_at, updated_at, phone, phone_confirmed_at, phone_change, phone_change_token, phone_change_sent_at, email_change_token_current, email_change_confirm_status, banned_until, reauthentication_token, reauthentication_sent_at)
+  values ('24a9b347-2170-41d4-a0e8-6078a9a0bef1', '00000000-0000-0000-0000-000000000000', 'authenticated', 'authenticated', 'test@test.com', '$2a$10$SmjgCFk1FYnEyrb.BCi9Te9puhz39SKBpW8yYyQWXD5UerE4O2pL.', timezone('utc'::text, now()), NULL, '', NULL, '', NULL, '', '', NULL, NULL, '{"provider": "email", "providers": ["email"]}', '{}', NULL, timezone('utc'::text, now()), timezone('utc'::text, now()), NULL, NULL, '', '', NULL, '', 0, NULL, '', NULL);
+
+insert into auth.identities (id, user_id, identity_data, provider, last_sign_in_at, created_at, updated_at)
+  values ('24a9b347-2170-41d4-a0e8-6078a9a0bef1', '24a9b347-2170-41d4-a0e8-6078a9a0bef1',	'{"sub":"24a9b347-2170-41d4-a0e8-6078a9a0bef1","email":"test@test.com"}', 'test@test.com', timezone('utc'::text, now()), timezone('utc'::text, now()), timezone('utc'::text, now()));
+
+insert into public.users (id, email, role)
+  values ('24a9b347-2170-41d4-a0e8-6078a9a0bef1', 'test@test.com', 'annotater'::"public"."user_roles")
