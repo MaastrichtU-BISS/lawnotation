0b5b28da484f3908210f52cac6dd428af37ff31e
eensander <50321538+eensander@users.noreply.github.com>
2023-09-20 16:51:47 +0200

    fix: remove tr from labelset table slot

--- lawnotation-ui/pages/labelset/index.vue
+++ lawnotation-ui/pages/labelset/index.vue
@@ -3,34 +3,26 @@
     <h3 class="text-lg font-semibold mb-2">Labelsets</h3>
     <Table
       endpoint="labelsets"
-      :columns="{
-        'Id': 'id',
-        'Name': 'name',
-        'Description': 'desc',
-        'Action': null
-      }"
       :sort="true"
       :search="true">
       <template #row="{item}: {item: Labelset}">
-        <tr class="bg-white border-b hover:bg-gray-50">
-          <th scope="row" class="px-6 py-2 font-medium text-gray-900 whitespace-nowrap">
-            {{ item.id }}
-          </th>
-          <td class="px-6 py-2">
-            {{ item.name }}
-          </td>
-          <td class="px-6 py-2">
-            {{ item.desc }}
-          </td>
-          <td class="px-6 py-2">
-            <NuxtLink
-              class="base"
-              :to="`/labelset/${item.id}`"
-            >
-              View
-            </NuxtLink>
-          </td>
-        </tr>
+        <th scope="row" class="px-6 py-2 font-medium text-gray-900 whitespace-nowrap">
+          {{ item.id }}
+        </th>
+        <td class="px-6 py-2">
+          {{ item.name }}
+        </td>
+        <td class="px-6 py-2">
+          {{ item.desc }}
+        </td>
+        <td class="px-6 py-2">
+          <NuxtLink
+            class="base"
+            :to="`/labelset/${item.id}`"
+          >
+            View
+          </NuxtLink>
+        </td>
       </template>
     </Table>
     <div class="my-4">
