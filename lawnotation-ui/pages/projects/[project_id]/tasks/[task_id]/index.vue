00781293ea1ffa1fd279c5cc957b0be751d36e0d
eensander <50321538+eensander@users.noreply.github.com>
2023-09-20 16:38:19 +0200

    chore: small type-script related changes in task-page

--- lawnotation-ui/pages/projects/[project_id]/tasks/[task_id]/index.vue
+++ lawnotation-ui/pages/projects/[project_id]/tasks/[task_id]/index.vue
@@ -19,98 +19,98 @@
 
   <div v-if="task">
     <div v-show="assignmentTable?.total">
-          <div class="text-center my-3">
-            <NuxtLink :to="`/projects/${task.project_id}/tasks/${task.id}/metrics`">
-              <button class="base btn-primary">Analyze Agreement Metrics</button>
-            </NuxtLink>
-          </div>
-          <h3 class="my-3 text-lg font-semibold">Assignments</h3>
-          <Table
+      <div class="text-center my-3">
+        <NuxtLink :to="`/projects/${task.project_id}/tasks/${task.id}/metrics`">
+          <button class="base btn-primary">Analyze Agreement Metrics</button>
+        </NuxtLink>
+      </div>
+      <h3 class="my-3 text-lg font-semibold">Assignments</h3>
+      <Table
         ref="assignmentTable"
         endpoint="assignments"
         :filter="{ task_id: task?.id }"
-            :sort="true"
-            :search="true"
+        :sort="true"
+        :search="true"
         :selectable="true"
-            @remove-rows="removeAssignments"
-            @remove-all-rows="removeAllAssignments"
+        @remove-rows="removeAssignments"
+        @remove-all-rows="removeAllAssignments"
+      >
+        <template #row="{ item }: { item: AssignmentTableData }">
+          <td
+            scope="row"
+            class="px-6 py-2 font-medium text-gray-900 whitespace-nowrap"
           >
-            <template #row="{ item }: { item: AssignmentTableData }">
-                <td
-                  scope="row"
-                  class="px-6 py-2 font-medium text-gray-900 whitespace-nowrap"
-                >
-                  {{ item.id }}
-                </td>
-                <td class="px-6 py-2">
-                  {{ item.annotator.email }}
-                </td>
-                <td class="px-6 py-2">
-                  {{ item.document.name }}
-                </td>
-                <td class="px-6 py-2">
-                  <span
-                    :class="item.status == 'done' ? 'text-green-600' : 'text-orange-700'"
-                    >{{ item.status }}</span
-                  >
-                </td>
-                <td class="px-6 py-2">
-                  <span>{{ item.difficulty_rating }}</span>
-                </td>
-                <td class="px-6 py-2">
-                  <NuxtLink class="base" :to="`/assignments/${item.id}`"> View </NuxtLink>
-                </td>
-            </template>
-          </Table>
-        </div>
+            {{ item.id }}
+          </td>
+          <td class="px-6 py-2">
+            {{ item.annotator.email }}
+          </td>
+          <td class="px-6 py-2">
+            {{ item.document.name }}
+          </td>
+          <td class="px-6 py-2">
+            <span
+              :class="item.status == 'done' ? 'text-green-600' : 'text-orange-700'"
+              >{{ item.status }}</span
+            >
+          </td>
+          <td class="px-6 py-2">
+            <span>{{ item.difficulty_rating }}</span>
+          </td>
+          <td class="px-6 py-2">
+            <NuxtLink class="base" :to="`/assignments/${item.id}`"> View </NuxtLink>
+          </td>
+        </template>
+      </Table>
+    </div>
     <div v-show="!assignmentTable?.total" class="flex justify-center">
-          <div class="flex flex-col w-1/2 space-y-2 border-neutral-300">
-            <h3 class="mt-3 text-lg font-semibold text-center">Create assignments</h3>
-            <h3 class="mt-3 text-sm font-semibold">
-              Annotators: {{ annotators_email.length }}
-            </h3>
-            <ul class="list-disc list-inside">
-              <li v-for="ann_email in annotators_email" :key="ann_email">
-                <span>{{ ann_email }}</span>
-              </li>
-            </ul>
-            <input
-              class="base"
-              id="annotator_email"
-              type="email"
-              name="email"
-              v-model="email"
-              @keydown.enter="addAnnotator()"
-            />
-            <button class="base btn-primary" @click="addAnnotator">Add</button>
-            <label for="amount_of_docs">Amount of Documents (total)</label>
-            <input
-              class="base"
-              id="amount_of_docs"
-              type="number"
-              name=""
-              v-model="amount_of_docs"
-              :max="total_docs"
-              min="1"
-            />
-            <label for="fixed_docs">
-              Amount of Fixed Documents (to share among annotators)
-            </label>
-            <input
-              class="base"
-              id="fixed_docs"
-              type="number"
-              name=""
-              v-model="amount_of_fixed_docs"
-              :max="total_docs"
-              min="0"
-            />
-            <button class="base btn-primary" @click="createAssignments">
-              Create Assignments
-            </button>
-          </div>
-        </div>
+      <div class="flex flex-col w-1/2 space-y-2 border-neutral-300">
+        <h3 class="mt-3 text-lg font-semibold text-center">Create assignments</h3>
+        <h3 class="mt-3 text-sm font-semibold">
+          Annotators: {{ annotators_email.length }}
+        </h3>
+        <ul class="list-disc list-inside">
+          <li v-for="ann_email in annotators_email" :key="ann_email">
+            <span>{{ ann_email }}</span>
+          </li>
+        </ul>
+        <input
+          class="base"
+          id="annotator_email"
+          type="email"
+          name="email"
+          v-model="email"
+          @keydown.enter="addAnnotator()"
+        />
+        <button class="base btn-primary" @click="addAnnotator">Add</button>
+        <label for="amount_of_docs">Amount of Documents (total)</label>
+        <input
+          class="base"
+          id="amount_of_docs"
+          type="number"
+          name=""
+          v-model="amount_of_docs"
+          :max="total_docs"
+          min="1"
+        />
+        <label for="fixed_docs">
+          Amount of Fixed Documents (to share among annotators)
+        </label>
+        <input
+          class="base"
+          id="fixed_docs"
+          type="number"
+          name=""
+          v-model="amount_of_fixed_docs"
+          :max="total_docs"
+          min="0"
+        />
+        <button class="base btn-primary" @click="createAssignments">
+          Create Assignments
+        </button>
       </div>
+    </div>
+  </div>
 </template>
 <script setup lang="ts">
 import {
@@ -198,14 +198,14 @@ const createAssignments = async () => {
     const annotators_id = (await Promise.all(usersPromises)).map((u) => u.id);
 
     // Assign users and order to assignments
-    let unshuffled: number[] = [
+    const unshuffled: number[] = [
       ...Array(
         amount_of_fixed_docs.value +
           (amount_of_docs.value - amount_of_fixed_docs.value) / annotators_id.length
       ).keys(),
     ];
 
-    let permutations = [];
+    const permutations = [];
     for (let i = 0; i < annotators_id.length; ++i) {
       permutations.push(shuffle(unshuffled));
     }
@@ -213,7 +213,7 @@ const createAssignments = async () => {
     const assignmentsPromises: Promise<Assignment>[] = [];
     for (let i = 0; i < created_assignments.length; ++i) {
       created_assignments[i].annotator_id = annotators_id[i % annotators_id.length];
-      created_assignments[i].seq_pos = permutations[i % annotators_id.length].pop() + 1;
+      created_assignments[i].seq_pos = permutations[i % annotators_id.length].pop() ?? 0 + 1;
       assignmentsPromises.push(
         $trpc.assignment.update.mutate({
           id: created_assignments[i].id,
