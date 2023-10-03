9c71966b026d56ea62f52247b388fde3b1ce58c9
CvdL-UM <c.vanderlans@maastrichtuniversity.nl>
2023-10-03 17:04:39 +0200

    Wait a little bit and then the menu item is found

--- lawnotation-ui/cypress/e2e/annotator.cy.js
+++ lawnotation-ui/cypress/e2e/annotator.cy.js
@@ -16,6 +16,7 @@ describe('Login annotator and test basic actions', () => {
 
   it('goes to index', () => {
     // (fetch)POST 200 /api/_supabase/session also messes up here
+    cy.wait(2000)
     cy.visit('/').get('a[data-test="assigned-taks-menu-item"]')
   })
 })
