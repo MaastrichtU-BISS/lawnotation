015852c0554be0cec07cd62bfb48dbd9191f1b5e
Carlos <carloseam94@gmail.com>
2023-10-25 12:37:01 +0200

    add: handle unauthorized error for projects

--- lawnotation-ui/server/trpc/index.ts
+++ lawnotation-ui/server/trpc/index.ts
@@ -7,9 +7,9 @@
  * @see https://trpc.io/docs/v10/router
  * @see https://trpc.io/docs/v10/procedures
  */
-import { TRPCError, initTRPC } from '@trpc/server'
-import { ZodError } from 'zod';
-import { Context } from '~/server/trpc/context'
+import { TRPCError, initTRPC } from "@trpc/server";
+import { ZodError } from "zod";
+import { Context } from "~/server/trpc/context";
 
 const t = initTRPC.context<Context>().create({
   errorFormatter(opts) {
@@ -18,56 +18,58 @@ const t = initTRPC.context<Context>().create({
       ...shape,
       data: {
         ...shape.data,
-        ...(
-          error.code === 'BAD_REQUEST' && error.cause instanceof ZodError
-            ? {zodErrors: error.cause.flatten()}
-            : {}
-        )
+        ...(error.code === "BAD_REQUEST" && error.cause instanceof ZodError
+          ? { zodErrors: error.cause.flatten() }
+          : {}),
       },
     };
   },
-})
+});
 
 /**
  * Middlewares
  */
 const isAuthenticated = t.middleware(({ next, ctx }) => {
   if (!ctx.user) {
-    throw new TRPCError({ code: 'UNAUTHORIZED' })
+    throw new TRPCError({ code: "UNAUTHORIZED" });
   }
 
   return next({
     ctx: {
       user: ctx.user,
     },
-  })
-})
+  });
+});
 
-
-type MiddlewareOptsParam = Parameters<Parameters<typeof middleware>[0]>[0]
-export const authorizer = async <T extends MiddlewareOptsParam>(opts: T, resolver: undefined | ((opts: T) => Promise<boolean>)) => {
-
+type MiddlewareOptsParam = Parameters<Parameters<typeof middleware>[0]>[0];
+export const authorizer = async <T extends MiddlewareOptsParam>(
+  opts: T,
+  resolver: undefined | ((opts: T) => Promise<boolean>)
+) => {
   try {
     let authorized = false;
-    if (opts.ctx.user?.role == 'admin')
-      authorized = true;
-    else if (resolver)
-      authorized = await resolver(opts);
+    if (opts.ctx.user?.role == "admin") authorized = true;
+    else if (resolver) authorized = await resolver(opts);
 
     if (!authorized)
-      throw new TRPCError({ code: 'NOT_FOUND' })
-      // throw new TRPCError({ code: 'FORBIDDEN' })
+      // throw new TRPCError({ code: 'NOT_FOUND' })
+      throw new TRPCError({
+        code: "FORBIDDEN",
+        message: "Sorry, the content you are looking for is not available.",
+      });
   } catch (error) {
     if (error instanceof TRPCError) {
       throw error;
     } else {
-      throw new TRPCError({ code: 'INTERNAL_SERVER_ERROR', message: 'Unable to authorize request' })
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Unable to authorize request",
+      });
     }
   }
 
   return opts.next();
-}
-
+};
 
 /**
  * Procedures
