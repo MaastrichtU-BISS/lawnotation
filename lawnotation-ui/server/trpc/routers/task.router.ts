82f960165f9bba4b552e62eef019264737aa73d9
Carlos <carloseam94@gmail.com>
2023-10-25 13:32:31 +0200

    add: project and task guards in task page

--- lawnotation-ui/server/trpc/routers/task.router.ts
+++ lawnotation-ui/server/trpc/routers/task.router.ts
@@ -1,147 +1,198 @@
-import { TRPCError } from '@trpc/server';
-import { z } from 'zod'
-import { protectedProcedure, router } from '~/server/trpc'
-import type { Annotation, Task } from '~/types';
-import { appRouter } from '.';
-import type { Database } from '~/types/supabase';
+import { TRPCError } from "@trpc/server";
+import { z } from "zod";
+import { authorizer, protectedProcedure, router } from "~/server/trpc";
+import type { Annotation, Task } from "~/types";
+import type { Context } from "../context";
+import { appRouter } from ".";
+import type { Database } from "~/types/supabase";
 
 const ZTaskFields = z.object({
   name: z.string(),
   desc: z.string(),
   project_id: z.number().int(),
   labelset_id: z.number().int(),
-  ann_guidelines: z.string()
+  ann_guidelines: z.string(),
 });
+
+const taskAuthorizer = async (
+  task_id: number,
+  user_id: string,
+  ctx: Context
+) => {
+  const { count } = await ctx.supabase
+    .from("tasks")
+    .select("*, project:projects!inner(editor_id)", {
+      count: "exact",
+      head: true,
+    })
+    .eq("id", task_id)
+    .eq("projects.editor_id", user_id);
+  return count === 1;
+};
 
 export const taskRouter = router({
   /* General Crud Definitions */
-  'find': protectedProcedure
-    .input(
-      z.object({
-        range: z.tuple([z.number().int(), z.number().int()]).optional(),
-        filter: ZTaskFields.partial().optional()
-      })
-    )
-    .query(async ({ ctx, input }) => {
-      let query = ctx.supabase.from("tasks").select();
-      if (input.range)
-        query = query.range(input.range[0], input.range[1])
-      if (input.filter)
-        query = query.match(input.filter)
-      
-      const { data, error } = await query;
-      
-      if (error)
-        throw new TRPCError({code: "INTERNAL_SERVER_ERROR", message: `Error in tasks.find: ${error.message}`});
-      return data as Task[];
-    }),
+  // find: protectedProcedure
+  //   .input(
+  //     z.object({
+  //       range: z.tuple([z.number().int(), z.number().int()]).optional(),
+  //       filter: ZTaskFields.partial().optional(),
+  //     })
+  //   )
+  //   .query(async ({ ctx, input }) => {
+  //     let query = ctx.supabase.from("tasks").select();
+  //     if (input.range) query = query.range(input.range[0], input.range[1]);
+  //     if (input.filter) query = query.match(input.filter);
 
-  'findById': protectedProcedure
+  //     const { data, error } = await query;
+
+  //     if (error)
+  //       throw new TRPCError({
+  //         code: "INTERNAL_SERVER_ERROR",
+  //         message: `Error in tasks.find: ${error.message}`,
+  //       });
+  //     return data as Task[];
+  //   }),
+
+  findById: protectedProcedure
     .input(z.number().int())
-    .query(async ({ ctx, input: id }) => {
-      const { data, error } = await ctx.supabase.from("tasks").select().eq('id', id).single();
-      
-      if (error)
-        throw new TRPCError({code: "INTERNAL_SERVER_ERROR", message: `Error in tasks.findById: ${error.message}`});
-      return data as Task;
-    }),
-
-  'create': protectedProcedure
-    .input(
-      ZTaskFields
+    .use((opts) =>
+      authorizer(opts, () =>
+        taskAuthorizer(opts.input, opts.ctx.user.id, opts.ctx)
+      )
     )
-    .mutation(async ({ ctx, input }) => {
-      const { data, error } = await ctx.supabase.from("tasks").insert(input).select().single();
-      
+    .query(async ({ ctx, input: id }) => {
+      const { data, error, count } = await ctx.supabase
+        .from("tasks")
+        .select()
+        .eq("id", id)
+        .single();
+
+      if (count === 0) throw new TRPCError({ code: "NOT_FOUND" });
       if (error)
-        throw new TRPCError({code: "INTERNAL_SERVER_ERROR", message: `Error in tasks.create: ${error.message}`});
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: `Error in tasks.findById: ${error.message}`,
+        });
       return data as Task;
     }),
 
-  'update': protectedProcedure
+  create: protectedProcedure
+    .input(ZTaskFields)
+    .mutation(async ({ ctx, input }) => {
+      const { data, error } = await ctx.supabase
+        .from("tasks")
+        .insert(input)
+        .select()
+        .single();
+
+      if (error)
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: `Error in tasks.create: ${error.message}`,
+        });
+      return data as Task;
+    }),
+
+  update: protectedProcedure
     .input(
       z.object({
         id: z.number().int(),
-        updates: ZTaskFields.partial()
+        updates: ZTaskFields.partial(),
       })
     )
     .mutation(async ({ ctx, input }) => {
-      const { data, error } = await ctx.supabase.from("tasks").update(input.updates).eq('id', input.id).select().single();
+      const { data, error } = await ctx.supabase
+        .from("tasks")
+        .update(input.updates)
+        .eq("id", input.id)
+        .select()
+        .single();
 
       if (error)
-        throw new TRPCError({code: "INTERNAL_SERVER_ERROR", message: `Error in tasks.update: ${error.message}`});
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: `Error in tasks.update: ${error.message}`,
+        });
       return data as Task;
     }),
 
-  'delete': protectedProcedure
+  delete: protectedProcedure
     .input(z.number().int())
     .mutation(async ({ ctx, input }) => {
-      const { error } = await ctx.supabase.from("tasks").delete().eq('id', input);
+      const { error } = await ctx.supabase
+        .from("tasks")
+        .delete()
+        .eq("id", input);
 
       if (error)
-        throw new TRPCError({code: "INTERNAL_SERVER_ERROR", message: `Error in tasks.delete: ${error.message}`});
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: `Error in tasks.delete: ${error.message}`,
+        });
       return true;
     }),
 
   // Extra procedures
 
-  'getCountByUser': protectedProcedure
-    .input(
-      z.string()
-    )
+  getCountByUser: protectedProcedure
+    .input(z.string())
     .query(async ({ ctx, input: editor_id }) => {
       const { data, error } = await ctx.supabase
         .rpc("get_count_tasks", { e_id: editor_id })
         .single();
 
       if (error)
-        throw new TRPCError({code: "INTERNAL_SERVER_ERROR", message: `Error in tasks.getCountByUser: ${error.message}`});
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: `Error in tasks.getCountByUser: ${error.message}`,
+        });
       return data as number;
     }),
 
-  'getAllAnnotatorTasks': protectedProcedure
-    .input(
-      z.string()
-    )
-    .query(async ({ctx, input: annotator_id}) => {
-      const { data, error } = await ctx.supabase.rpc("get_all_annotator_tasks", {
-        a_id: annotator_id,
-      });
+  getAllAnnotatorTasks: protectedProcedure
+    .input(z.string())
+    .query(async ({ ctx, input: annotator_id }) => {
+      const { data, error } = await ctx.supabase.rpc(
+        "get_all_annotator_tasks",
+        {
+          a_id: annotator_id,
+        }
+      );
       if (error)
-        throw new TRPCError({code: "INTERNAL_SERVER_ERROR", message: `Error in tasks.getAllAnnotatorTasks: ${error.message}`});
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: `Error in tasks.getAllAnnotatorTasks: ${error.message}`,
+        });
       return data as Task[];
     }),
 
-  'deleteAllFromProject': protectedProcedure
-    .input(
-      z.number().int()
-    )
-    .mutation(async ({ctx, input: project_id}) => {
+  deleteAllFromProject: protectedProcedure
+    .input(z.number().int())
+    .mutation(async ({ ctx, input: project_id }) => {
       const { data, error } = await ctx.supabase
         .from("tasks")
         .delete()
         .eq("project_id", project_id);
 
       if (error)
-        throw new TRPCError({code: "INTERNAL_SERVER_ERROR", message: `Error in tasks.delete: ${error.message}`});
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: `Error in tasks.delete: ${error.message}`,
+        });
       return true;
     }),
 
   // Note: wont work, probably
-  'replicateTask': protectedProcedure 
-    .input(
-      z.number().int()
-    )
-    .mutation(async ({ctx, input: task_id}): Promise<Task> => {
-
+  replicateTask: protectedProcedure
+    .input(z.number().int())
+    .mutation(async ({ ctx, input: task_id }): Promise<Task> => {
       const caller = appRouter.createCaller(ctx);
-      
-      
 
       // const annotationApi = useAnnotationApi();
       // const assignmentApi = useAssignmentApi();
       // const relationApi = useAnnotationRelationApi();
-  
+
       const task = await caller.task.findById(task_id);
       const new_task = await caller.task.create({
         name: task.name,
@@ -150,8 +201,10 @@ export const taskRouter = router({
         ann_guidelines: task.ann_guidelines,
         labelset_id: task.labelset_id,
       });
-  
-      const assignments = await caller.assignment.findAssignmentsByTask(task_id);
+
+      const assignments = await caller.assignment.findAssignmentsByTask(
+        task_id
+      );
       const new_assignments = await caller.assignment.createMany(
         assignments.map((a) => {
           return {
@@ -164,7 +217,7 @@ export const taskRouter = router({
           };
         })
       );
-  
+
       let dicAssignments: any = {};
       new_assignments.map((na, index) => {
         dicAssignments[assignments[index].id] = na.id;
@@ -173,11 +226,13 @@ export const taskRouter = router({
       type NonNullableObject<T> = {
         [K in keyof T]: NonNullable<T[K]>;
       };
-  
+
       console.log(assignments.length, new_assignments.length);
-  
-      const annotations = await caller.annotation.findAnnotationsByTask(task_id);
-  
+
+      const annotations = await caller.annotation.findAnnotationsByTask(
+        task_id
+      );
+
       const new_annotations = await caller.annotation.createMany(
         annotations.map((a) => {
           return {
@@ -191,16 +246,16 @@ export const taskRouter = router({
           };
         })
       );
-  
+
       console.log(annotations.length, new_annotations.length);
-  
+
       const relations = await caller.relation.findRelationsByTask(task_id);
-  
+
       let dicAnnotations: any = {};
       new_annotations.map((na, index) => {
         dicAnnotations[annotations[index].id] = na.id;
       });
-  
+
       const new_relations = await caller.relation.createMany(
         relations.map((a) => {
           return {
@@ -213,13 +268,12 @@ export const taskRouter = router({
           };
         })
       );
-  
+
       console.log(relations.length, new_relations.length);
 
       // return 1;
       return new_task;
-    })
+    }),
+});
 
-})
-
-export type LabelsetRouter = typeof taskRouter
+export type LabelsetRouter = typeof taskRouter;
