d1c94034cf33d07e07d46ae058f8a5d825cc349d
Carlos <carloseam94@gmail.com>
2023-11-09 19:47:17 +0100

    add: export task modal

--- lawnotation-ui/pages/projects/[project_id]/tasks/[task_id]/index.vue
+++ lawnotation-ui/pages/projects/[project_id]/tasks/[task_id]/index.vue
@@ -17,80 +17,73 @@
   <div class="my-3 dimmer-wrapper">
     <Dimmer v-model="loading" />
     <div class="dimmer-content">
-      <div v-if="task">
-        <div v-show="assignmentTable?.total">
-          <div class="text-center my-3">
-            <NuxtLink :to="`/projects/${task.project_id}/tasks/${task.id}/metrics`">
-              <button
-                class="mx-3 rounded-md bg-primary px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-primary/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slate-600">
-                Analyze Agreement Metrics
-              </button>
-            </NuxtLink>
-            <!-- <button
-              class="mx-3 rounded-md bg-primary px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-primary/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slate-600"
-              @click="replicateTask">
-              Replicate Task
-            </button> -->
+      <div v-show="assignmentTable?.total">
+        <div class="text-center my-3">
+          <NuxtLink :to="`/projects/${task?.project_id}/tasks/${task?.id}/metrics`">
             <button
-              class="mx-3 rounded-md bg-primary px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-primary/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slate-600"
-              @click="exportTask">
-              Export Task
+              class="mx-3 rounded-md bg-primary px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-primary/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slate-600">
+              Analyze Agreement Metrics
             </button>
-          </div>
-          <h3 class="my-3 text-lg font-semibold">Assignments</h3>
-          <Table ref="assignmentTable" endpoint="assignments" :filter="{ task_id: task?.id }" :sort="true" :search="true"
-            :selectable="true" @remove-rows="removeAssignments" @remove-all-rows="removeAllAssignments">
-            <template #row="{ item }: { item: AssignmentTableData }">
-              <td scope="row" class="px-6 py-2 font-medium text-gray-900 whitespace-nowrap">
-                {{ item.id }}
-              </td>
-              <td class="px-6 py-2">
-                {{ item.annotator.email }}
-              </td>
-              <td class="px-6 py-2">
-                {{ item.document.name }}
-              </td>
-              <td class="px-6 py-2">
-                <span :class="item.status == 'done' ? 'text-green-600' : 'text-orange-700'">{{ item.status }}</span>
-              </td>
-              <td class="px-6 py-2">
-                <span>{{ item.difficulty_rating }}</span>
-              </td>
-              <td class="px-6 py-2">
-                <NuxtLink class="base" :to="`/assignments/${item.id}`"> View </NuxtLink>
-              </td>
-            </template>
-          </Table>
+          </NuxtLink>
+          <button type="button" data-modal-target="exportFormModal" data-modal-toggle="exportFormModal"
+            class="mx-3 rounded-md bg-primary px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-primary/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slate-600">
+            Export Task
+          </button>
         </div>
-        <div v-show="!assignmentTable?.total" class="flex justify-center">
-          <div class="flex flex-col w-1/2 space-y-2 border-neutral-300">
-            <h3 class="mt-3 text-lg font-semibold text-center">Create assignments</h3>
-            <h3 class="mt-3 text-sm font-semibold">
-              Annotators: {{ annotators_email.length }}
-            </h3>
-            <ul class="list-disc list-inside">
-              <li v-for="ann_email in annotators_email" :key="ann_email">
-                <span>{{ ann_email }}</span>
-              </li>
-            </ul>
-            <input class="base" id="annotator_email" type="email" name="email" v-model="email"
-              @keydown.enter="addAnnotator()" />
-            <button class="base btn-primary" @click="addAnnotator">Add</button>
-            <label for="amount_of_docs">Amount of Documents (total)</label>
-            <input class="base" id="amount_of_docs" type="number" name="" v-model="amount_of_docs" :max="total_docs"
-              min="1" />
-            <label for="fixed_docs">
-              Amount of Fixed Documents (to share among annotators)
-            </label>
-            <input class="base" id="fixed_docs" type="number" name="" v-model="amount_of_fixed_docs" :max="total_docs"
-              min="0" />
-            <button class="base btn-primary" @click="createAssignments">
-              Create Assignments
-            </button>
-          </div>
+        <h3 class="my-3 text-lg font-semibold">Assignments</h3>
+        <Table ref="assignmentTable" endpoint="assignments" :filter="{ task_id: task?.id }" :sort="true" :search="true"
+          :selectable="true" @remove-rows="removeAssignments" @remove-all-rows="removeAllAssignments">
+          <template #row="{ item }: { item: AssignmentTableData }">
+            <td scope="row" class="px-6 py-2 font-medium text-gray-900 whitespace-nowrap">
+              {{ item.id }}
+            </td>
+            <td class="px-6 py-2">
+              {{ item.annotator.email }}
+            </td>
+            <td class="px-6 py-2">
+              {{ item.document.name }}
+            </td>
+            <td class="px-6 py-2">
+              <span :class="item.status == 'done' ? 'text-green-600' : 'text-orange-700'">{{ item.status }}</span>
+            </td>
+            <td class="px-6 py-2">
+              <span>{{ item.difficulty_rating }}</span>
+            </td>
+            <td class="px-6 py-2">
+              <NuxtLink class="base" :to="`/assignments/${item.id}`"> View </NuxtLink>
+            </td>
+          </template>
+        </Table>
+      </div>
+      <div v-show="!assignmentTable?.total" class="flex justify-center">
+        <div class="flex flex-col w-1/2 space-y-2 border-neutral-300">
+          <h3 class="mt-3 text-lg font-semibold text-center">Create assignments</h3>
+          <h3 class="mt-3 text-sm font-semibold">
+            Annotators: {{ annotators_email.length }}
+          </h3>
+          <ul class="list-disc list-inside">
+            <li v-for="ann_email in annotators_email" :key="ann_email">
+              <span>{{ ann_email }}</span>
+            </li>
+          </ul>
+          <input class="base" id="annotator_email" type="email" name="email" v-model="email"
+            @keydown.enter="addAnnotator()" />
+          <button class="base btn-primary" @click="addAnnotator">Add</button>
+          <label for="amount_of_docs">Amount of Documents (total)</label>
+          <input class="base" id="amount_of_docs" type="number" name="" v-model="amount_of_docs" :max="total_docs"
+            min="1" />
+          <label for="fixed_docs">
+            Amount of Fixed Documents (to share among annotators)
+          </label>
+          <input class="base" id="fixed_docs" type="number" name="" v-model="amount_of_fixed_docs" :max="total_docs"
+            min="0" />
+          <button class="base btn-primary" @click="createAssignments">
+            Create Assignments
+          </button>
         </div>
       </div>
     </div>
+    <ExportTaskModal v-model="export_options" @export="exportTask"></ExportTaskModal>
   </div>
 </template>
 <script setup lang="ts">
@@ -99,6 +92,8 @@ import Table from "~/components/Table.vue";
 import { shuffle, clone } from "lodash";
 import { authorizeClient } from "~/utils/authorize.client";
 import { downloadAs } from "~/utils/download_file";
+import { ExportTaskOptions } from "~/utils/io"
+import { initFlowbite } from "flowbite";
 
 const { $toast, $trpc } = useNuxtApp();
 
@@ -111,6 +106,15 @@ const total_docs = ref<number>(0);
 const amount_of_docs = ref<number>(0);
 const amount_of_fixed_docs = ref<number>(0);
 const annotators_email = reactive<string[]>([]);
+const export_options = ref<ExportTaskOptions>({
+  name: true,
+  desc: true,
+  ann_guidelines: true,
+  labelset: true,
+  documents: false,
+  annotations: false
+})
+
 const loading = ref(false);
 
 const email = ref("");
@@ -294,6 +298,7 @@ const exportTask = async () => {
 }
 
 onMounted(async () => {
+  initFlowbite();
   task.value = await $trpc.task.findById.query(+route.params.task_id);
 
   $trpc.document.totalAmountOfDocs.query(+task.value.project_id).then((count) => {
