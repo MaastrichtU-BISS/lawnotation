ac742cec0e8ee7e0dbf2a58b5a69ccf33191223e
CvdL-UM <93982741+CvdL-UM@users.noreply.github.com>
2023-12-19 16:52:22 +0100

    Add Terms of Service and Privacy Policy (#25)
    * Added a first draft for the privacy policy and link to it from the login screen
    
    * Updated privacy policy
    
    * Added Terms of Service
    
    * Added link to Terms of Service on Login screen
    
    * Update Privacy Policy

---
+++ lawnotation-ui/pages/terms-of-service.vue
@@ -0,0 +1,324 @@
+<template>
+  <div class="bg-gray-50">
+    <div class="mx-auto max-w-screen-lg py-16">
+      <h1>Terms of Service</h1>
+      <ol class="list-decimal mt-6">
+        <li>
+          <h2>Applicability Terms of Service</h2>
+          <p>
+            The following terms and conditions govern all use of the Lawnotation software
+            and services, subsequently referred to as “Lawnotation”.
+          </p>
+          <p>
+            By accessing or using any part of Lawnotation you agree with the content and
+            applicability of these terms and conditions.
+          </p>
+          <p>
+            The applicability of any and all terms and conditions of a user of Lawnotation
+            are hereby expressly rejected.
+          </p>
+          <p>
+            If any provision of these terms and conditions is null and void or is
+            annulled, the other provisions of these terms and conditions shall remain in
+            full force and effect.
+          </p>
+        </li>
+        <li>
+          <h2>License to use Lawnotation</h2>
+          <p>
+            Lawnotation is owned and operated by Maastricht University and is made
+            available open source under an
+            <a href="https://choosealicense.com/licenses/apache-2.0/" target="_blank"
+              >Apache License 2.0</a
+            >. The open source software used in Lawnotation is provided according to its
+            terms.
+          </p>
+          <p>
+            Provided that a user of Lawnotation complies with these terms and conditions,
+            Maastricht University grants that user a personal, non-exclusive,
+            non-transferable, non-sublicensable and revocable (as provided herein) license
+            to use Lawnotation.
+          </p>
+          <p>
+            Unless indicated otherwise, Maastricht University does not charge a license
+            fee for the use of Lawnotation in accordance with these terms and conditions.
+          </p>
+        </li>
+        <li>
+          <h2>Lawnotation account</h2>
+          <p>
+            The user of Lawnotation is not permitted to provide access to his/her account
+            to third parties or to others than the relevant user of Lawnotation to use the
+            relevant Lawnotation account. The user shall not create more accounts than
+            necessary.
+          </p>
+          <p>
+            The user of Lawnotation is obligated to immediately notify Maastricht
+            University via
+            <a href="mailto:servicedesk-icts@maastrichtuniversity.nl"
+              >servicedesk-icts@maastrichtuniversity.nl</a
+            >
+            of any unauthorized use(s) of his/her account.
+          </p>
+          <p>
+            Maastricht University may discontinue, limit or temporarily interrupt access
+            of any and all users of Lawnotation at any time without being liable for
+            damages as a result thereof.
+          </p>
+          <p>
+            Maastricht University has not reviewed, and cannot review, all of the data,
+            including computer software, made available through Lawnotation.
+            Notwithstanding aforementioned Maastricht University has the right to remove
+            any data that is in any way harmful or objectionable at any time without being
+            liable for damages as a result thereof.
+          </p>
+        </li>
+        <li>
+          <h2>Restrictions</h2>
+          <p class="!mb-0">The user of Lawnotation may not:</p>
+          <ol class="list-[lower-latin]">
+            <li>
+              <p>
+                Disassemble, modify, prepare derivative works of, decompile or reverse
+                engineer Lawnotation or permit or authorize a third party to do so;
+              </p>
+            </li>
+            <li>
+              <p>
+                Remove, disable or otherwise circumvent any security features that
+                Lawnotation might contain;
+              </p>
+            </li>
+            <li>
+              <p>
+                Delete or alter any copyright, trademark, and other proprietary rights
+                notices or markings appearing on Lawnotation;
+              </p>
+            </li>
+            <li>
+              <p>
+                Infringe the proprietary rights, including but not limited to the
+                copyright, patent, trademark or trade secret rights, of Maastricht
+                University and/or any third party relating to Lawnotation;
+              </p>
+            </li>
+            <li>
+              <p>Hack Lawnotation or use unauthorized APIs;</p>
+            </li>
+            <li>
+              <p>
+                Install any viruses, worms, time bombs, malware, Trojan horses or other
+                harmful or destructive content, code, files, scripts or agents on
+                Lawnotation;
+              </p>
+            </li>
+            <li>
+              <p>
+                Use Lawnotation for spam or any other unethical or unwanted commercial
+                content designed to drive traffic to third party sites or boost the search
+                engine rankings of third party sites, or to further unlawful acts (such as
+                phishing) or mislead recipients as to the source of the data (such as
+                spoofing);
+              </p>
+            </li>
+            <li>
+              <p>
+                Use Lawnotation in any manner or for any purpose that infringes,
+                misappropriates, or other otherwise violates any other party’s rights,
+                these Terms of Service or applicable law.
+              </p>
+            </li>
+          </ol>
+        </li>
+        <li>
+          <h2>Ownership</h2>
+          <p>
+            By using Lawnotation no intellectual property rights of Maastricht University
+            or any third party will be transferred to the user of Lawnotation. All right,
+            title and interest in and to such property will remain solely with Maastricht
+            University and/or such third party.
+          </p>
+          <p>
+            The use of Lawnotation grants the user of Lawnotation no right or license to
+            reproduce or otherwise use any Maastricht University or third-party
+            trademarks.
+          </p>
+          <p>
+            If a user of Lawnotation makes any suggestion, comment, idea, recommendation
+            and/or feedback regarding the Lawnotation, Maastricht University is free to
+            use such suggestion, comment, idea, recommendation and/or feedback and the
+            user of Lawnotation assigns to Maastricht University that suggestion, comment,
+            idea, recommendation and/or feedback and any and all intellectual property
+            rights therein.
+          </p>
+        </li>
+        <li>
+          <h2>Alleged copyright infringement</h2>
+          <p>
+            Although Maastricht University is not responsible and/or liable for the data
+            that is displayed in Lawnotation, Maastricht University does not wish to
+            facilitate any infringement of intellectual property rights.
+          </p>
+          <p>
+            Therefore, in case you believe that any data displayed in the Lawnotation
+            violates your intellectual property rights, you are encouraged to notify
+            Maastricht University without undue delay. Maastricht University will respond
+            to all such notices, including as required or appropriate by removing the
+            infringing data or disabling all links to the infringing data.
+          </p>
+        </li>
+        <li>
+          <h2>Confidentiality</h2>
+          <p>
+            The user of Lawnotation agrees to keep all information, documents and data,
+            which is marked or which would reasonably be considered to be confidential
+            (“Confidential Information”) as strictly confidential and to use such
+            information only within the scope of the licenses granted to the user of
+            Lawnotation.
+          </p>
+          <p>
+            To avoid doubt; Lawnotation and all its information, documents and data in the
+            broadest sense of the word concerns Maastricht University’s Confidential
+            Information.
+          </p>
+        </li>
+        <li>
+          <h2>Processing of personal data</h2>
+          <p>
+            Lawnotation and Maastricht University explicitly disclaim any and all
+            liability in connection with the processing of personal data provided by the
+            user of Lawnotation. In the event that personal data, whether belonging to the
+            user or any other natural persons, or other information of a confidential or
+            sensitive nature, is transmitted to and/or stored to the Lawnotation platform,
+            it is hereby emphasized that the user, as the party providing said data, shall
+            bear full accountability for all aspects of data processing, irrespective of
+            whether the user has a data processing agreement for this data or not.
+          </p>
+        </li>
+        <li>
+          <h2>No warranties Lawnotation use</h2>
+          <p>
+            Maastricht University does not warrant that Lawnotation will meet your
+            requirements, will operate in the combination that you desire, will operate
+            error-free or uninterrupted, or that all Lawnotation errors will be corrected.
+          </p>
+          <p>
+            Maastricht University expressly disclaims and makes no representation,
+            warranty, condition or other contractual term, express or implied, including
+            but not limited to any implied representations, warranties, conditions or
+            contractual terms of merchantability, fitness for a particular purpose and
+            noninfringement, and any representations, warranties, conditions or
+            contractual terms arising out of course of dealing or usage.
+          </p>
+        </li>
+        <li>
+          <h2>Limitation of Liability</h2>
+          <p>
+            Maastricht University disclaims any responsibility for the use of Lawnotation
+            and any data used therein.
+          </p>
+          <p>
+            Maastricht University will in no event, even if Maastricht University is
+            apprised of the likelihood of such damages occurring, be liable for any harm
+            resulting from the use of Lawnotation including but not limited to (i) any
+            special, incidental or consequential damages; (ii) the cost of procurement for
+            substitute products or services; (iii) for interruption of use or loss or
+            corruption of data; (iiii) any loss of profits or loss of business.
+          </p>
+          <p>
+            The foregoing limitation of liability shall not apply to the extent prohibited
+            by applicable law.
+          </p>
+        </li>
+        <li>
+          <h2>General Representation and Warranty</h2>
+          <p>
+            The user of Lawnotation represents and warrants that their use of Lawnotation
+            will be in strict accordance with these terms and conditions, the UM
+            Acceptable Use Policy, and with all applicable laws and regulations (including
+            without limitation any local laws or regulations in your country, state, city,
+            or other governmental area, regarding online conduct and acceptable content).
+          </p>
+        </li>
+        <li>
+          <h2>Indemnification</h2>
+          <p>
+            The user of Lawnotation agrees to defend, indemnify and hold harmless
+            Maastricht University and their respective directors, officers, employees and
+            agents from and against any and all liabilities, damages, costs, and expenses,
+            including settlement costs and attorneys’ fees, from any claim or demand by a
+            third party arising out of their use of Lawnotation, including but not limited
+            to their violation of these terms and conditions, the UM Acceptable Use
+            Policy, and/or any applicable laws and regulations or any third party rights.
+          </p>
+        </li>
+        <li>
+          <h2>Termination</h2>
+          <p>
+            The licenses to use the Lawnotation granted under these terms and conditions
+            will automatically terminate, with or without notice from Maastricht
+            University, if the user of Lawnotation breaches any of these terms and
+            conditions, the UM Acceptable Use Policy, and/or any applicable laws and
+            regulations.
+          </p>
+          <p>
+            Upon termination, the user of Lawnotation must either promptly destroy or
+            return to Maastricht University the Lawnotation in his/her possession or
+            control.
+          </p>
+          <p>
+            All provisions of these terms and conditions which by their nature should
+            survive termination shall survive termination, including, without limitation,
+            ownership provisions, warranty disclaimers, indemnity, limitations of
+            liability and governing law.
+          </p>
+        </li>
+        <li>
+          <h2>Amendments to terms and conditions</h2>
+          <p>
+            Maastricht University reserves the right to amend and/or supplement these
+            terms and conditions at any time.
+          </p>
+          <p>
+            The continued use of Lawnotation following the posting of any changes to these
+            terms and conditions constitutes acceptance of those amendments.
+          </p>
+        </li>
+        <li>
+          <h2>Miscellaneous</h2>
+          <p>These terms and conditions are governed by Dutch law.</p>
+          <p>
+            All disputes arising from, or resulting from the use of Lawnotation which
+            cannot be resolved by mutual agreement, will be submitted to the competent
+            court of the District Court of Limburg, the Netherlands.
+          </p>
+        </li>
+      </ol>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: "blank",
+});
+</script>
+
+<style lang="postcss" scoped>
+h1 {
+  @apply font-bold text-4xl;
+}
+h2,
+h1 + ol > li::marker {
+  @apply font-bold text-2xl;
+}
+p {
+  @apply mb-4 mt-2;
+}
+p + p {
+  @apply -mt-2;
+}
+a {
+  @apply underline;
+}
+</style>
