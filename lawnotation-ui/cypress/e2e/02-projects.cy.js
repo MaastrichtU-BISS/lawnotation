df25e96a4d4fb000bce109afb7e2850c80e780af
CvdL-UM <c.vanderlans@maastrichtuniversity.nl>
2024-01-29 11:09:18 +0100

    change: Formatting

--- lawnotation-ui/cypress/e2e/02-projects.cy.js
+++ lawnotation-ui/cypress/e2e/02-projects.cy.js
@@ -10,19 +10,22 @@ describe('Navigate to projects and test functionality ', () => {
     it('Enter projects and Add, Delete and Edit ', () => {
         cy.get('a[data-test="projects-link"]').click()
         cy.wait(1000)
+
         cy.addProject('Project1')
         cy.addProject('Project2')
         cy.addProject('Project3')
         cy.addProject('Project4')
+
         cy.get('a[data-test="edit-project-link"]').eq(0).click()
         cy.addTask('Task1')
         cy.addTask('Task2')
         cy.addTask('Task3')
         cy.addTask('Task4')
+
         cy.wait(1000)
-        cy.get('[data-test="Checkbox"]').eq(0).check()
+        cy.get('input[data-test="checkbox"]').eq(0).check()
         cy.wait(1000)
-        cy.get('[data-test="Checkbox"]').eq(2).check()
+        cy.get('input[data-test="checkbox"]').eq(2).check()
         cy.wait(1000)
         cy.get('button[data-test="remove-selected-rows"]').eq(1).click({ force: true })   //test if the project is deleted
         cy.wait(1000)
@@ -30,11 +33,13 @@ describe('Navigate to projects and test functionality ', () => {
         cy.wait(1000)
         cy.reload()
         cy.get('a[data-test="View-task"]').should("have.length", 2)
+
         cy.wait(1000)
         cy.get('button[data-test="remove-all"]').eq(1).click({ force: true })  //test if the project is deleted
         cy.get('button').contains('Confirm').click()
         cy.reload()
         cy.get('a[data-test="View-task"]').should("have.length", 0)
+
         cy.get('a[data-test="projects-link"]').click()
         cy.wait(1000)
         cy.get('[data-test="checkbox"]').eq(0).check()
@@ -44,6 +49,7 @@ describe('Navigate to projects and test functionality ', () => {
         cy.get('button[data-test="remove-selected-rows"]').eq(0).click({ force: true })  //test if the project is deleted
         cy.reload()
         cy.get('a[data-test="edit-project-link"]').should("have.length", 2)  //test if the project is deleted
+        
         cy.reload()
         cy.wait(1000)
         cy.get('button[data-test="remove-all"]').eq(1).click({ force: true })
