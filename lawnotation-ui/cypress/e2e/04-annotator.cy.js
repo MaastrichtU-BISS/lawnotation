7348e6918d2bcf3bf7c8dc67eeb16c867eeca6a1
CvdL-UM <c.vanderlans@maastrichtuniversity.nl>
2024-01-30 13:35:37 +0100

    change: Rename

--- lawnotation-ui/cypress/e2e/04-annotator.cy.js
+++ lawnotation-ui/cypress/e2e/04-annotator.cy.js
@@ -4,9 +4,8 @@ describe('Login annotator and test basic actions', () => {
   });
 
   it('goes to index', () => {
-    // (fetch)POST 200 /api/_supabase/session also messes up here
-    cy.wait(2000)
-    cy.visit('/').get('a[data-test="assigned-tasks-menu-item"]')
+    cy.wait(3000)
+    cy.get('a[data-test="assigned-tasks-menu-item"]')
     cy.get('button[data-test="start-annotating-button"]').should('not.exist')
   })
 })
