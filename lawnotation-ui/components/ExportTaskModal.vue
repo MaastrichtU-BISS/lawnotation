00c94016b891ff849d9aad01e71826a8f556cda5
Carlos <carloseam94@gmail.com>
2024-02-05 23:31:09 +0100

    fix: guidelines url optional

--- lawnotation-ui/components/ExportTaskModal.vue
+++ lawnotation-ui/components/ExportTaskModal.vue
@@ -136,7 +136,7 @@
                   class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
                   File url</label>
               </div>
-              <div v-if="modelValue.export_options.ann_guidelines" class="relative z-0 w-full mb-5 group">
+              <div class="relative z-0 w-full mb-5 group">
                 <input v-model="modelValue.publication.guidelines_url" type="url" name="guidelines_url" id="guidelines_url"
                   class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                   placeholder=" " />
@@ -277,17 +277,22 @@ const publish = async () => {
     return;
   }
 
-  if (props.modelValue.export_options.ann_guidelines && !props.modelValue.publication.guidelines_url) {
-    $toast.error("Guidelines url is required");
-    return;
+  if (props.modelValue.export_options.ann_guidelines) {
+
+    if(!props.modelValue.publication.guidelines_url) {
+      $toast.error("Guidelines url is required");
+      return;
+    }
+
+    try {
+      const url = new URL(props.modelValue.publication.guidelines_url);
+    } catch (_) {
+      $toast.error("Invalid Guidelines url");
+      return;
+    }
   }
 
-  try {
-    const url = new URL(props.modelValue.publication.guidelines_url);
-  } catch (_) {
-    $toast.error("Invalid Guidelines url");
-    return;
-  }
+
 
   if (!props.modelValue.publication.author) {
     $toast.error("Author is required");
