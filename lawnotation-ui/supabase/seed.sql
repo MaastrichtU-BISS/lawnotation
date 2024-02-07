874cbd035b93dc611c638fb83b046132b599e28a
CvdL-UM <c.vanderlans@maastrichtuniversity.nl>
2024-02-07 16:29:17 +0100

    change: Table structure needed for newer Supabase version

--- lawnotation-ui/supabase/seed.sql
+++ lawnotation-ui/supabase/seed.sql
@@ -6,7 +6,7 @@ values
   ('24a9b347-2170-41d4-a0e8-6078a9a0bef2', '00000000-0000-0000-0000-000000000000', 'authenticated', 'authenticated', 'editor@test.com', '$2a$10$SmjgCFk1FYnEyrb.BCi9Te9puhz39SKBpW8yYyQWXD5UerE4O2pL.', timezone('utc'::text, now()), NULL, '', NULL, '', NULL, '', '', NULL, NULL, '{"provider": "email", "providers": ["email"]}', '{}', NULL, timezone('utc'::text, now()), timezone('utc'::text, now()), NULL, NULL, '', '', NULL, '', 0, NULL, '', NULL);
 
 insert into auth.identities 
-  (id, user_id, identity_data, provider, last_sign_in_at, created_at, updated_at)
+  (provider_id, user_id, identity_data, provider, last_sign_in_at, created_at, updated_at)
 values 
   ('24a9b347-2170-41d4-a0e8-6078a9a0bef1', '24a9b347-2170-41d4-a0e8-6078a9a0bef1',	'{"sub":"24a9b347-2170-41d4-a0e8-6078a9a0bef1","email":"annotator@test.com"}', 'annotator@test.com', timezone('utc'::text, now()), timezone('utc'::text, now()), timezone('utc'::text, now())),
   ('24a9b347-2170-41d4-a0e8-6078a9a0bef2', '24a9b347-2170-41d4-a0e8-6078a9a0bef2',	'{"sub":"24a9b347-2170-41d4-a0e8-6078a9a0bef2","email":"editor@test.com"}', 'editor@test.com', timezone('utc'::text, now()), timezone('utc'::text, now()), timezone('utc'::text, now()));
