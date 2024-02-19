3df7c306ffc20a478af49212ea0273d77b5c808a
CvdL-UM <93982741+CvdL-UM@users.noreply.github.com>
2024-02-19 17:16:49 +0100

    Merge 'fix e2e testing' to 'dev' branch (#28)
    * update: Nuxt and Cypress
    
    * Disable devtools since they seem to mess with e2e testing
    
    * Use new 6-digit login flow
    
    * change: a user -> an account
    
    * fix: Test
    
    * refactor: Move data-test from svg to button
    
    * add: Data-test for label
    
    * refactor: No longer reset db since the labelset is removed at the end of the test
    
    * refactor: Make sure label1 is removed and saved and that the labelset is removed at the end
    
    * refactor: No longer reset db, since the projects are all removed at the end of the test
    
    * refactor: Add a non-existing annotator so the invite email can be manually verified in inbucket
    
    * refactor: Downgrade Nuxt to current production version, since the latest version throws a 500 error after building

--- lawnotation-ui/cypress/e2e/02-labels.cy.ts
+++ lawnotation-ui/cypress/e2e/02-labels.cy.ts
@@ -1,10 +1,6 @@
 import Role from '../support/role';
 
 describe('Test label functionality', () => {
-  before(() => {
-    cy.resetDatabase();
-  });
-
   beforeEach(() => {
     cy.login(Role.EDITOR);
   });
@@ -12,21 +8,38 @@ describe('Test label functionality', () => {
   it('Enters label studio and tests functionality', () => {
     cy.wait(3000)
     cy.get('a[data-test="labelset-link"]').click()
+    
+    cy.wait(2000)
     cy.get('button[data-test="create-new-labelset"]').click()
     cy.get('input[data-test="labelset-name"]').type('test1')
     cy.get('textarea[data-test="labelset-description"]').type('test1')
     cy.get('input[data-test="label-name"]').type('test1')
     cy.get('button[data-test="add-label"]').click()
     cy.get('button[data-test="create-labelset"]').click()
-    cy.get('a[data-test="edit-labelset"]').first().click() // start editing label
+
+    cy.get('a[data-test="edit-labelset"]').first().click()
     cy.get('input[data-test="edit-labelset-name"]').clear().type('test1.1')
     cy.get('textarea[data-test="edit-labelset-description"]').clear().type('test1.1')
-    cy.get('input[data-test="label-name"]').type('test101')
+    cy.get('input[data-test="label-name"]').type('test2')
     cy.get('button[data-test="Add-label"]').click()
     cy.get('button[data-test="Save-changes"]').click()
+
     cy.get('a[data-test="labelset-link"]').click()
-    cy.get('a[data-test="edit-labelset"]').first().click() // start deleting label
-    cy.get('svg[data-test="delete-label"]').first().click()
+    cy.get('a[data-test="edit-labelset"]').first().click()
+    cy.get('span[data-test="label"]').contains('test1').should('exist')
+    cy.get('button[data-test="delete-label"]').first().click()
+    cy.get('span[data-test="label"]').contains('test1').should('not.exist')
+    cy.get('button[data-test="Save-changes"]').click()
+
     cy.get('a[data-test="labelset-link"]').click()
+    cy.get('a[data-test="edit-labelset"]').first().click()
+    cy.get('span[data-test="label"]').contains('test1').should('not.exist')
+    
+    cy.get('a[data-test="labelset-link"]').click()
+    cy.get('td').contains('test1.1').should('exist')
+    cy.get('input[data-test="checkbox"').first().click()
+    cy.get('button[data-test="remove-selected-rows"').click()
+    cy.get('button').contains('Confirm').click()
+    cy.get('td').contains('test1.1').should('not.exist')
   })
 })
