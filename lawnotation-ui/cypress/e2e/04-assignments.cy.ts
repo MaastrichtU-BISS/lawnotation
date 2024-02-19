0e840448370d0dcfeb3198f9b96c6b2887f36823
CvdL-UM <c.vanderlans@maastrichtuniversity.nl>
2024-02-19 16:16:11 +0100

    refactor: Add a non-existing annotator so the invite email can be manually verified in inbucket

--- lawnotation-ui/cypress/e2e/04-assignments.cy.ts
+++ lawnotation-ui/cypress/e2e/04-assignments.cy.ts
@@ -26,13 +26,17 @@ describe('Assign a project to the annotator account as the editor', () => {
         cy.contains('Create assignments').should('exist')
         cy.get('input[id="annotator_email"]').type('annotator@test.com')
         cy.get('button').contains('Add').click()
+        cy.get('input[id="annotator_email"]').type('annotator1@test.com')
+        cy.get('button').contains('Add').click()
         cy.get('button').contains('Create Assignments').click()
 
         cy.get('td').contains('annotator@test.com').should('exist')
+        cy.get('td').contains('annotator1@test.com').should('not.exist')
         cy.get('td').contains('Test.txt').should('exist')
         cy.get('#breadcrumb-holder').find('li').eq(1).click()
         cy.get('button[data-test="view-task-button"]').click()
         cy.get('td').contains('annotator@test.com').should('exist')
+        cy.get('td').contains('annotator1@test.com').should('not.exist')
         cy.get('td').contains('Test.txt').should('exist')
         
         cy.get('a').contains('View').first().click()
