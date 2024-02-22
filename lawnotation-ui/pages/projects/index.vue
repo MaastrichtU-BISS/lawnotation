e4d0bcd25ed6427458f6a105a7f9bb5754457e65
Carlos <carloseam94@gmail.com>
2024-02-22 14:40:33 +0100

    feat: modal to create project

--- lawnotation-ui/pages/projects/index.vue
+++ lawnotation-ui/pages/projects/index.vue
@@ -1,15 +1,11 @@
 <template>
   <div class="max-w-screen-lg mx-auto my-4">
-    <h3 class="mb-2 text-lg font-semibold">Projects</h3>
-    <Table
-      endpoint="projects"
-      ref="projectTable"
-      :sort="true"
-      :search="true"
-      :selectable="true"
-      @remove-rows="removeProjects"
-      @remove-all-rows="removeAllProjects"
-    >
+    <div class="flex justify-between">
+      <h3 class="mb-2 text-lg font-semibold">Projects</h3>
+      <Button label="Add" icon="pi pi-plus" @click="showCreateModal = true" icon-pos="right" />
+    </div>
+    <Table endpoint="projects" ref="projectTable" :sort="true" :search="true" :selectable="true"
+      @remove-rows="removeProjects" @remove-all-rows="removeAllProjects">
       <template #row="{ item }: { item: Project }">
         <td scope="row" class="px-6 py-2 font-medium text-gray-900 whitespace-nowrap">
           {{ item.id }}
@@ -18,38 +14,34 @@
           {{ item.name }}
         </td>
         <td class="px-6 py-2">
-          <NuxtLink data-test="edit-project-link" class="base" :to="`/projects/${item.id}`"> 
-            <button class="base btn-primary">View</button>
+          <NuxtLink data-test="edit-project-link" class="mr-2" :to="`/projects/${item.id}`">
+            <Button label="View" size="small" />
+          </NuxtLink>
+          <NuxtLink :to="`/projects/${item.id}/edit`" data-test="edit-project-link">
+            <Button label="Edit" size="small" link />
           </NuxtLink>
         </td>
       </template>
     </Table>
-
-    <h3 class="mt-8 text-lg">Create new project</h3>
-    <div class="flex flex-col w-1/2 pt-3 mt-3 space-y-2 border-t border-neutral-300">
-      <input
-        data-test="project-name"
-        class="base"
-        type="text"
-        placeholder="Project name"
-        v-model="new_project.name"
-      />
-      <textarea
-        data-test="project-description"
-        class="base"
-        placeholder="Project description"
-        v-model="new_project.desc"
-      ></textarea>
-      <button data-test="add-project" class="flex-none base btn-primary" @click="createNewProject">Add</button>
-    </div>
+    <Dialog v-model:visible="showCreateModal" modal header="Create new project">
+      <div class="flex justify-center mb-4">
+        <span class="relative w-full">
+          <InputText v-model="new_project.name" data-test="project-name" id="project_name" autocomplete="off" class="peer w-full" placeholder="" mode="filled"/>
+          <label for="project_name"
+            class="absolute text-sm text-primary-500 dark:text-primary-400/60 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">Name</label>
+        </span>
+      </div>
+      <Textarea v-model="new_project.desc" data-test="project-description" autoResize rows="5" cols="30" placeholder="Description" class="w-full mb-4"/>
+      <div class="flex justify-between mt-4">
+        <Button label="Cancel" icon="pi pi-times" iconPos="right" severity="secondary" @click="showCreateModal = false"/>
+        <Button data-test="add-project" label="Create" icon="pi pi-check" iconPos="right" @click="createNewProject"/>
+      </div>
+    </Dialog>
   </div>
 </template>
 <script setup lang="ts">
 import type { Project } from "~/types";
 import Table from "@/components/Table.vue";
-import { TRPCClientError } from "@trpc/client";
-import { TRPCError } from "@trpc/server";
-import type { ZodError, typeToFlattenedError } from "zod";
 
 const projectTable = ref<InstanceType<typeof Table> | null>();
 
@@ -62,14 +54,24 @@ const new_project = reactive<Omit<Project, "id">>({
   editor_id: "",
 });
 
+const showCreateModal = ref<boolean>(false);
+
+watch(showCreateModal, (new_val) => {
+  if(!new_val) {
+    new_project.name = new_project.desc = "";
+  }
+})
+
 const createNewProject = async () => {
   try {
     new_project.editor_id = user.value?.id!;
-    const project = await $trpc.project.create.mutate(new_project)
+    await $trpc.project.create.mutate(new_project)
     projectTable.value?.refresh()
     $toast.success("Project created");
   } catch (error) {
     trpcErrorHandler(error as Error, "creating new project")
+  } finally {
+    showCreateModal.value = false;
   }
 };
 
