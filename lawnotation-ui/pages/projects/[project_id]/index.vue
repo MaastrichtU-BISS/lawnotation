cf6fb20122ba6a255e4f4e9117f3507afe8397a2
CvdL-UM <c.vanderlans@maastrichtuniversity.nl>
2024-03-13 14:08:02 +0100

    refactor: Cleanup

--- lawnotation-ui/pages/projects/[project_id]/index.vue
+++ lawnotation-ui/pages/projects/[project_id]/index.vue
@@ -58,7 +58,7 @@
                   style: 'padding-bottom: 0px'
                 }
               }">
-                <TabView v-model:active-index="activeTabTaskModal" class="min-h-[540px]">
+                <TabView :active-index="activeTabTaskModal" class="min-h-[540px]">
                   <TabPanel header="New">
                     <div class="flex justify-center mb-4">
                       <span class="relative w-full">
