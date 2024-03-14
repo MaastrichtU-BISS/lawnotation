916542800c95a13398f80d9b8eaa44775c16be80
eensander <50321538+eensander@users.noreply.github.com>
2024-03-14 15:36:32 +0100

    feat: add reload command after disabling openapi in PostgREST

--- lawnotation-ui/supabase/migrations/20240314085233_revoke_privileges.sql
+++ lawnotation-ui/supabase/migrations/20240314085233_revoke_privileges.sql
@@ -1,7 +1,10 @@
 -- inspired from: https://github.com/orgs/supabase/discussions/21647#discussion-6289771
 
+-- disable public openapi page
 ALTER ROLE authenticator SET pgrst.openapi_mode TO 'disabled';
+NOTIFY pgrst, 'reload config';
 
+-- revoke access to tables from anon and authenticated
 REVOKE USAGE ON SCHEMA public FROM anon, authenticated;
 REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA public FROM anon, authenticated;
 REVOKE ALL PRIVILEGES ON ALL ROUTINES IN SCHEMA public FROM anon, authenticated;
