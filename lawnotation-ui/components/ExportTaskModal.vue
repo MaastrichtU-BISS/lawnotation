40e180321176e6369373473b1b20b7cd7c6a155a
CvdL-UM <c.vanderlans@maastrichtuniversity.nl>
2024-03-25 17:29:48 +0100

    fix: Author is required

--- lawnotation-ui/components/ExportTaskModal.vue
+++ lawnotation-ui/components/ExportTaskModal.vue
@@ -194,7 +194,7 @@
                 <div class="relative z-0 w-full mb-5 group">
                   <input v-model="modelValue.publication.author" type="text" name="author" id="author"
                     class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                    placeholder=" " />
+                    placeholder=" " required />
                   <label for="author"
                     class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
                     Author</label>
