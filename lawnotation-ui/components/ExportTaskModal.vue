370b1d4a92d9b57a60a8f09e7515f248733f811c
CvdL-UM <c.vanderlans@maastrichtuniversity.nl>
2024-03-25 10:36:00 +0100

    refactor: Fixed logic

--- lawnotation-ui/components/ExportTaskModal.vue
+++ lawnotation-ui/components/ExportTaskModal.vue
@@ -55,7 +55,7 @@
                 </li>
                 <li class="w-full dark:border-gray-600">
                   <div class="flex items-center pl-3">
-                    <input id="labelset-checkbox" type="checkbox" @click="click_labelset" v-model="modelValue.export_options.labelset"
+                    <input id="labelset-checkbox" type="checkbox" @change="click_labelset" v-model="modelValue.export_options.labelset"
                       class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
                     <label for="labelset-checkbox"
                       class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Labels</label>
@@ -66,7 +66,7 @@
                 class="my-5 items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
                 <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
                   <div class="flex items-center pl-3">
-                    <input id="documments-checkbox" @click="click_documents" type="checkbox"
+                    <input id="documments-checkbox" @change="click_documents" type="checkbox"
                       v-model="modelValue.export_options.documents"
                       class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
                     <label for="documments-checkbox"
@@ -228,14 +228,14 @@ const page = ref<number>(1);
 const emit = defineEmits(["export", "close", "resetForm"]);
 
 const click_labelset = () => {
-  if (props.modelValue.export_options.labelset) {
+  if (!props.modelValue.export_options.labelset) {
     props.modelValue.export_options.ann_guidelines = false;
     props.modelValue.export_options.annotations = false;
   }
 };
 
 const click_documents = () => {
-  if (props.modelValue.export_options.documents) {
+  if (!props.modelValue.export_options.documents) {
     props.modelValue.export_options.annotations = false;
   }
 };
