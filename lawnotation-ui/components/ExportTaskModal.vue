d6b7b0f9dccaaefd40161ab52f51ee62b83da3aa
CvdL-UM <c.vanderlans@maastrichtuniversity.nl>
2024-03-25 10:33:49 +0100

    refactor: Replace buttons with PrimeVue buttons

--- lawnotation-ui/components/ExportTaskModal.vue
+++ lawnotation-ui/components/ExportTaskModal.vue
@@ -87,43 +87,34 @@
                 Annotations will be exported without the annotator email
               </p>
               <div class="flex justify-between my-5">
-                <button type="button" :disabled="!(
-                  modelValue.export_options.name ||
-                  modelValue.export_options.desc ||
-                  modelValue.export_options.ann_guidelines ||
-                  modelValue.export_options.labelset ||
-                  modelValue.export_options.documents ||
-                  modelValue.export_options.annotations)
-                  " @click="export_task"
-                  class="primary text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                  data-test="export-button">
-                  {{ modelValue.export_options.loading ? 'Downloading...' : 'Export' }}
-                  <svg v-if="modelValue.export_options.loading" aria-hidden="true" role="status"
-                    class="inline ml-2 w-4 h-4 me-3 text-white animate-spin" viewBox="0 0 100 101" fill="none"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path
-                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                      fill="#E5E7EB" />
-                    <path
-                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                      fill="currentColor" />
-                  </svg>
-                  <svg v-else class="ml-2 w-3.5 h-3.5 white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                    fill="none" viewBox="0 0 16 18">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                      d="M8 1v11m0 0 4-4m-4 4L4 8m11 4v3a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-3" />
-                  </svg>
-                </button>
-                <button :disabled="!modelValue.export_options.loaded" @click="page++" type="button"
-                  class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                  data-test="start-publishing-button">
-                  Start publishing
-                  <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                    fill="none" viewBox="0 0 14 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                      d="M1 5h12m0 0L9 1m4 4L9 9" />
-                  </svg>
-                </button>
+                <Button
+                  type="button"
+                  @click="export_task"
+                  :label="modelValue.export_options.loading ? 'Downloading...' : 'Export'"
+                  icon="pi pi-download"
+                  iconPos="right"
+                  :disabled="
+                    !(
+                      modelValue.export_options.name ||
+                      modelValue.export_options.desc ||
+                      modelValue.export_options.ann_guidelines ||
+                      modelValue.export_options.labelset ||
+                      modelValue.export_options.documents ||
+                      modelValue.export_options.annotations
+                    )
+                  "
+                  :outlined="modelValue.export_options.loaded"
+                  data-test="export-button"
+                />
+                <Button
+                  type="button"
+                  @click="page++"
+                  label="Start publishing"
+                  icon="pi pi-arrow-right"
+                  iconPos="right"
+                  :disabled="!modelValue.export_options.loaded"
+                  data-test="start-publishing-button"
+                />
               </div>
             </div>
             <div v-if="page == 2" id="export-step-2">
