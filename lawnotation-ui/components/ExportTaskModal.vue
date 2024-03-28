0172792f8d5d7e3695c82b440fe32e588a420f4f
CvdL-UM <c.vanderlans@maastrichtuniversity.nl>
2024-03-28 09:59:40 +0100

    refactor: Rename for better clarity

--- lawnotation-ui/components/ExportTaskModal.vue
+++ lawnotation-ui/components/ExportTaskModal.vue
@@ -147,7 +147,7 @@
             for="file_url"
             class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
-            File url</label
+            External file url</label
           >
         </div>
         <div class="relative z-0 w-full mb-5 group">
