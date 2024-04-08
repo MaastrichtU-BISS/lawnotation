d8a8d303a5eff2596bd7785c0550846048e58df2
Carlos <carloseam94@gmail.com>
2024-04-08 12:49:23 +0200

    feat: progress bar works (reloading page)

--- lawnotation-ui/nuxt.config.ts
+++ lawnotation-ui/nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
+  nitro: {
+    experimental: {
+      websockets: true,
+    }
+  }
   runtimeConfig: {
     public: {
       mlBackendURL: process.env.MLBACKEND_URL,
