4e6ef60c8292235a94534c6d003c9c837ac92a13
Carlos <carloseam94@gmail.com>
2024-04-08 13:44:47 +0200

    fix: remove experimental nitro ws

--- lawnotation-ui/nuxt.config.ts
+++ lawnotation-ui/nuxt.config.ts
@@ -1,11 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  nitro: {
-    experimental: {
-      websockets: true,
-    }
-  }
   runtimeConfig: {
     public: {
       mlBackendURL: process.env.MLBACKEND_URL,
