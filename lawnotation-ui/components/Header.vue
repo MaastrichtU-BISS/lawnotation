3f9ea2e982b77b0caedbc0022c812e017a0b6317
eensander <50321538+eensander@users.noreply.github.com>
2024-04-16 11:20:28 +0200

    remove old menu

--- lawnotation-ui/components/Header.vue
+++ lawnotation-ui/components/Header.vue
@@ -106,60 +106,6 @@
       </template>
     </Menubar>
   </div>
-
-
-  <div
-    v-if="false"
-    class="flex items-center justify-between px-4 py-4 border-b bg-neutral-50 border-neutral-200"
-  >
-    <div class="w-40">
-      <NuxtLink to="/"><img src="/lawnotation-logo.svg" /></NuxtLink>
-    </div>
-    <div v-if="user" class="space-x-4">
-      <NuxtLink
-        to="/projects"
-        class="header-link"
-        :class="{ active: routeIsActive('/projects') }"
-        data-test="projects-link"
-        >Projects</NuxtLink
-      >
-      <span class="text-gray-400 select-none">|</span>
-      <NuxtLink
-        to="/labelset"
-        class="header-link"
-        :class="{ active: routeIsActive('/labelset') }"
-        data-test="labelset-link"
-        >Labelsets</NuxtLink
-      >
-      <span class="text-gray-400 select-none">|</span>
-      <NuxtLink to="/published" class="header-link" :class="{'active': routeIsActive('/published')}">Published Data</NuxtLink>
-      <span class="text-gray-400 select-none">|</span>
-      <NuxtLink
-        to="/tasks"
-        class="header-link"
-        :class="{ active: routeIsActive('/tasks') }"
-        data-test="assigned-tasks-menu-item"
-        >Assigned Tasks</NuxtLink
-      >
-    </div>
-    <div class="auth">
-      <template v-if="user">
-        <span class="text-slate-800" v-if="user?.email">{{
-          user.email?.split("@")[0]
-        }}</span>
-        <span class="mx-3 text-gray-400 select-none">|</span>
-        <button class="header-link" @click="signOut()">Sign Out</button>
-      </template>
-      <template v-else>
-        <NuxtLink
-          class="header-link"
-          to="/auth/login"
-          :class="{ active: routeIsActive('/auth/login') }"
-          >Sign In</NuxtLink
-        >
-      </template>
-    </div>
-  </div>
 </template>
 <script setup lang="ts">
 import type Menu from 'primevue/menu';
