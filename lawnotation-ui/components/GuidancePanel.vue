c88a855d6d54fdb1bddf64a69df61122d37f09dc
Carlos <carloseam94@gmail.com>
2024-05-14 16:13:59 +0200

    feat: add guidance on the projects page

---
+++ lawnotation-ui/components/GuidancePanel.vue
@@ -0,0 +1,26 @@
+<template>
+    <Fieldset class="max-w-md mx-auto mb-4 text-sm" :pt="{
+        legend: {
+            class: ['p-3']
+        },
+        content: {
+            class: ['p-3 pt-0']
+        }
+    }">
+        <template #legend class="p-3">
+            <div class="flex items-center gap-2 px-2">
+                <i class="pi pi-info-circle text-yellow-300 animate-bounce" />
+                <span class="font-bold">{{ title }}</span>
+            </div>
+        </template>
+        <p class="m-0">
+            {{ text }}
+        </p>
+    </Fieldset>
+</template>
+<script setup lang="ts">
+const props = defineProps<{
+    title: string;
+    text: string;
+}>();
+</script>
--- lawnotation-ui/pages/projects/index.vue
+++ lawnotation-ui/pages/projects/index.vue
@@ -1,8 +1,14 @@
 <template>
-  <div class="max-w-screen-lg mx-auto my-4">
+  <div class="max-w-screen-lg mx-auto my-8">
+    <GuidancePanel v-if="projectTable?.total! <= 1" :title="guidanceTitle" :text="guidanceText"/>
     <div class="flex justify-between">
       <h3 class="mb-2 text-lg font-semibold">Projects</h3>
-      <Button label="Add" icon="pi pi-plus" @click="showCreateModal = true" icon-pos="right" data-test="open-projects-modal"/>
+      <div class="relative">
+        <Button label="Add" icon="pi pi-plus" @click="showCreateModal = true" icon-pos="right"
+          data-test="open-projects-modal" />
+          <PulsingRedCircle v-if="projectTable?.total! == 0" />
+      </div>
+
     </div>
     <Table endpoint="projects" ref="projectTable" :sort="true" :search="true" :selectable="true"
       @remove-rows="removeProjects" @remove-all-rows="removeAllProjects">
@@ -17,9 +23,12 @@
           {{ item.desc }}
         </td>
         <td class="px-6 py-2 flex">
-          <NuxtLink data-test="view-project-link" class="mr-2" :to="`/projects/${item.id}`">
-            <Button label="View" size="small" />
-          </NuxtLink>
+          <div class="relative mr-2">
+            <NuxtLink data-test="view-project-link" :to="`/projects/${item.id}`">
+              <Button label="View" size="small" />
+            </NuxtLink>
+            <PulsingRedCircle v-if="projectTable?.total! == 1" />
+          </div>
           <NuxtLink :to="`/projects/${item.id}/edit`" data-test="edit-project-link">
             <Button label="Edit" size="small" link />
           </NuxtLink>
@@ -29,15 +38,19 @@
     <Dialog v-model:visible="showCreateModal" modal header="Create new project">
       <div class="flex justify-center mb-4">
         <span class="relative w-full">
-          <InputText v-model="new_project.name" data-test="project-name" id="project_name" autocomplete="off" class="peer w-full" placeholder="" />
+          <InputText v-model="new_project.name" data-test="project-name" id="project_name" autocomplete="off"
+            class="peer w-full" placeholder="" />
           <label for="project_name"
             class="absolute text-sm text-primary-500 dark:text-primary-400/60 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">Name</label>
         </span>
       </div>
-      <Textarea v-model="new_project.desc" data-test="project-description" autoResize rows="5" cols="30" placeholder="Description" class="w-full mb-4"/>
+      <Textarea v-model="new_project.desc" data-test="project-description" autoResize rows="5" cols="30"
+        placeholder="Description" class="w-full mb-4" />
       <div class="flex justify-center mt-4">
-        <Button class="mr-6" label="Cancel" size="small" icon="pi pi-times" iconPos="right" outlined @click="showCreateModal = false"/>
-        <Button data-test="add-project" label="Create" size="small" icon="pi pi-check" iconPos="right" @click="createNewProject"/>
+        <Button class="mr-6" label="Cancel" size="small" icon="pi pi-times" iconPos="right" outlined
+          @click="showCreateModal = false" />
+        <Button data-test="add-project" label="Create" size="small" icon="pi pi-check" iconPos="right"
+          @click="createNewProject" />
       </div>
     </Dialog>
   </div>
@@ -45,6 +58,8 @@
 <script setup lang="ts">
 import type { Project } from "~/types";
 import Table from "@/components/Table.vue";
+import PulsingRedCircle from "~/components/PulsingRedCircle.vue";
+import GuidancePanel from "~/components/GuidancePanel.vue";
 
 const projectTable = ref<InstanceType<typeof Table> | null>();
 
@@ -59,8 +74,24 @@ const new_project = reactive<Omit<Project, "id">>({
 
 const showCreateModal = ref<boolean>(false);
 
+const guidanceText = computed(() => {
+  if(projectTable.value?.total == 0) {
+    return "The first step is to create a new project. To do that, click on the highlighted button and fill in the form."
+  } else if (projectTable.value?.total == 1) {
+    return "Now that you have a project, click on the highlighted button to continue."
+  }
+});
+
+const guidanceTitle = computed(() => {
+  if(projectTable.value?.total == 0) {
+    return "Create your first project"
+  } else if (projectTable.value?.total == 1) {
+    return "Go to the newly created project"
+  }
+});
+
 watch(showCreateModal, (new_val) => {
-  if(!new_val) {
+  if (!new_val) {
     new_project.name = new_project.desc = "";
   }
 })
