3d8a7f6fa5ff5bfa7bc1da02998fec2ae84ad73f
Carlos <carloseam94@gmail.com>
2024-05-14 16:05:24 +0200

    refactor: make more noticeable the color change on hover. (index cards)

--- lawnotation-ui/components/IndexCard.vue
+++ lawnotation-ui/components/IndexCard.vue
@@ -1,6 +1,6 @@
 <template>
     <a :href="url"
-        class="flex items-center justify-center max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-50 text-gray-500 hover:text-gray-600 transition ease-in-out duration-300 md:min-h-[250px] md:min-w-[300px]">
+        class="flex items-center justify-center max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 text-gray-500 hover:text-gray-600 transition ease-in-out duration-300 md:min-h-[250px] md:min-w-[300px]">
         <div class="text-center">
             <h5 class="mb-6 text-2xl font-bold tracking-tight">{{ title }}</h5>
             <svg class="inline-block w-16 h-16" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
