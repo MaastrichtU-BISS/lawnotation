d574d9796615bafce318c506253d8c0c5ef49a45
Carlos <carloseam94@gmail.com>
2024-05-31 15:18:21 +0200

    fix: copy migrations from dev

---
+++ lawnotation-ui/supabase/migrations/20231027095629_remote_schema.sql
@@ -0,0 +1,55 @@
+alter table "public"."users" drop constraint "users_id_fkey";
+
+alter table "public"."users" alter column "role" drop default;
+
+alter type "public"."user_roles" rename to "user_roles__old_version_to_be_dropped";
+
+create type "public"."user_roles" as enum ('editor', 'annotator', 'admin');
+
+alter table "public"."users" alter column role type "public"."user_roles" using role::text::"public"."user_roles";
+
+alter table "public"."users" alter column "role" set default 'annotator'::user_roles;
+
+drop type "public"."user_roles__old_version_to_be_dropped";
+
+alter table "public"."users" alter column "role" set default 'editor'::user_roles;
+
+alter table "public"."users" add constraint "users_id_fkey" FOREIGN KEY (id) REFERENCES auth.users(id) not valid;
+
+alter table "public"."users" validate constraint "users_id_fkey";
+
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.get_unannotated_documents(task_id_param bigint)
+ RETURNS TABLE(document_id bigint)
+ LANGUAGE plpgsql
+AS $function$
+BEGIN
+    RETURN QUERY
+    SELECT DISTINCT a.document_id
+    FROM assignments a
+    JOIN tasks t ON a.task_id = t.id
+    WHERE t.id = task_id_param
+    AND NOT EXISTS (
+        SELECT 1
+        FROM annotations an
+        WHERE an.assignment_id = a.id
+    );
+END;
+$function$
+;
+
+CREATE OR REPLACE FUNCTION public.handle_new_user()
+ RETURNS trigger
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$
+begin
+  insert into public.users (id, email)
+  values (new.id, new.email);
+  return new;
+end;
+$function$
+;
+
+
---
+++ lawnotation-ui/supabase/migrations/20240304154635_remote_schema.sql
@@ -0,0 +1,43 @@
+CREATE INDEX annotation_relations_from_id ON public.annotation_relations USING btree (from_id);
+
+CREATE INDEX annotation_relations_to_id ON public.annotation_relations USING btree (to_id);
+
+CREATE INDEX annotations_assignment_id ON public.annotations USING btree (assignment_id);
+
+CREATE INDEX assignments_task_id ON public.assignments USING btree (task_id);
+
+CREATE INDEX publications_task_name_labels_name_author_contact_task_desc_idx ON public.publications USING btree (task_name, labels_name, author, contact, task_description, labels_description);
+
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.get_unannotated_documents(task_id_param bigint)
+ RETURNS TABLE(document_id bigint)
+ LANGUAGE plpgsql
+AS $function$
+BEGIN
+    RETURN QUERY
+    SELECT DISTINCT a.document_id
+    FROM assignments a
+    JOIN tasks t ON a.task_id = t.id
+    WHERE t.id = task_id_param
+    AND NOT EXISTS (
+        SELECT 1
+        FROM annotations an
+        WHERE an.assignment_id = a.id
+    );
+END;
+$function$
+;
+
+CREATE OR REPLACE FUNCTION public.handle_new_user()
+ RETURNS trigger
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$
+begin
+  insert into public.users (id, email)
+  values (new.id, new.email);
+  return new;
+end;
+$function$
+;
