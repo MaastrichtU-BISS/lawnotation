410381f2e11a37d2078721da7e554d117042496a
Carlos <carloseam94@gmail.com>
2024-07-04 12:37:52 +0200

    feat: create archives page

---
+++ lawnotation-ui/components/SearchDocuments.vue
@@ -0,0 +1,6 @@
+<template>
+    search documents
+</template>
+
+<script setup lang="ts">
+</script>
---
+++ lawnotation-ui/pages/archives/index.vue
@@ -0,0 +1,8 @@
+<template>
+<div class="max-w-screen-lg mx-auto my-8">
+<SearchDocuments />
+</div>
+</template>
+<script setup lang="ts">
+import SearchDocuments from "../../components/SearchDocuments.vue";
+</script>
