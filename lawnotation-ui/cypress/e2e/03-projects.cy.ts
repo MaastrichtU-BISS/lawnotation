e9283f184d07366d99d62f4a9adc52fa1db9085e
CvdL-UM <c.vanderlans@maastrichtuniversity.nl>
2024-08-22 12:13:41 +0200

    refactor: Fix test

--- lawnotation-ui/cypress/e2e/03-projects.cy.ts
+++ lawnotation-ui/cypress/e2e/03-projects.cy.ts
@@ -84,18 +84,18 @@ describe('Testing projects and tasks with the editor account', () => {
     })
 
     it('Add a labelset when creating a new task', () => {
-        cy.wait(3000)
+        cy.visit('/')
         cy.get('a[data-test="projects-link"]').click()
         cy.wait(1000)
         cy.addProject('Labelset test')
 
         cy.get('a[data-test="view-project-link"]').eq(0).click()
+        cy.get('a[data-test="tasks-tab"]').click();
         cy.get('button[data-test="open-tasks-modal"]').click();
         cy.get('input[data-test="task-name"]').type("Task with new labelset")
         cy.get('textarea[data-test="task-description"]').type('test task')
-        cy.get('textarea[data-test="annotation-guidelines"]').clear().type('1.{enter}2.{enter}3.{enter}')
-        cy.get('button[data-test="create-new-labelset"]').first().click()
-        cy.get('button[data-test="create-new-labelset"]').last().click()
+        cy.get('input[data-test="annotation-guidelines"]').clear().type('https://example.com')
+        cy.get('button[data-test="create-new-labelset"]').click()
 
         cy.get('input[data-test="labelset-name"]').type('Labelset for task')
         cy.get('textarea[data-test="labelset-description"]').type('test1')
@@ -111,11 +111,12 @@ describe('Testing projects and tasks with the editor account', () => {
 
         cy.wait(3000)
         cy.get('a[data-test="projects-link"]').click()
-        cy.get('button[data-test="remove-all"]').click()
+        cy.get('button[data-test="remove-all-menu-button"]').click()
+        cy.get('div[data-test="remove-all"]').click()
         cy.get('button').contains('Confirm').click()
         cy.get('a[data-test="view-project-link"]').should("have.length", 0)
 
-        cy.get('a[data-test="labelsets-link"]').click()
+        cy.get('a[data-test="labelset-link"]').click()
         cy.get('[data-test="checkbox"]').eq(0).check()
         cy.get('button[data-test="remove-selected-rows"]').click()
         cy.get('button').contains('Confirm').click()
