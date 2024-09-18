a685cdf01e3be9046fedd75976b581a312d1238d
Parisxx <91888826+Parisxx@users.noreply.github.com>
2024-09-18 13:30:24 +0200

    feat: editor creates and deletes

---
+++ lawnotation-ui/tests/02-labelsets-loggedin.spec.ts
@@ -0,0 +1,38 @@
+import { test, expect } from '@playwright/test';
+import { delay } from './utils';
+import path from 'node:path';
+
+
+test('Editor creates, edits, and deletes labelset', async ({ browser }) => {
+    test.setTimeout(120000);
+    const editorContext = await browser.newContext({ storageState: 'playwright/.auth/editor.json' });
+    const editorPage = await editorContext.newPage();
+    await editorPage.goto('localhost:3000');
+    await editorPage.getByTestId('labelset-link').click();
+    await editorPage.getByTestId('create-new-labelset').click();
+    await editorPage.getByLabel('Breadcrumb').waitFor();
+    await editorPage.getByTestId('labelset-name').fill('Test labelset');
+    await editorPage.getByTestId('labelset-description').fill('Labelset description');
+    await editorPage.getByTestId('label-name').fill('First label');
+    await editorPage.getByTestId('add-label').click();
+    await expect(editorPage.getByText('First label')).toBeVisible();
+    await editorPage.getByTestId('label-name').fill('Second label');
+    await editorPage.getByTestId('add-label').click();
+    await expect(editorPage.getByText('Second label')).toBeVisible();
+    await editorPage.getByTestId('delete-label').last().click();
+    await expect(editorPage.getByText('Second label')).not.toBeVisible();
+    await editorPage.getByTestId('label-name').fill('Second label');
+    await editorPage.getByTestId('add-label').click();
+    await expect(editorPage.getByText('Second label')).toBeVisible();
+    await editorPage.getByTestId('save-labelset').click();
+    await expect(editorPage.getByRole('table').locator('tbody').locator('tr')).toHaveCount(3);
+    await editorPage.getByTestId('checkbox').first().check();
+    await editorPage.getByTestId('remove-selected-rows').click();
+    await editorPage.getByText('Confirm').click();
+    await expect(editorPage.getByRole('table').locator('tbody').locator('tr')).toHaveCount(2);
+});
+
+
+
+
+
