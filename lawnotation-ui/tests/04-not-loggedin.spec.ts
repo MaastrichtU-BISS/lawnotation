8b1c1ef1b629b9d727938a462a6b7c0e4a81920b
CvdLans <93982741+CvdL-UM@users.noreply.github.com>
2024-09-26 13:20:21 +0200

    refactor: Fix test by using pressSequentially instead of fill

--- lawnotation-ui/tests/04-not-loggedin.spec.ts
+++ lawnotation-ui/tests/04-not-loggedin.spec.ts
@@ -6,11 +6,11 @@ test('Not logged in user goes to documentation and downloads an ECLI', async ({ 
     await page.goto('localhost:3000/archives');
     await expect(page.getByText("Provide ECLIs", { exact: true })).toBeVisible();
 
-    await page.getByTestId('eclis').fill('ECLI:NL:RBLIM:2023:7197');
-    await page.getByTestId('eclis').press('Tab');
-    await delay(3000);
-
+    const eclisInput = page.getByTestId('eclis');
+    await eclisInput.pressSequentially('ECLI:NL:RBLIM:2023:7197', { delay: 100 });
+    await eclisInput.press('Enter');
+    
     await page.getByTestId('download-button').click();
-
+    await delay(3000);
 });
 
