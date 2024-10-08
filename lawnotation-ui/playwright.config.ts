0754201177aaaca7bdd36c4c5c29e32b14613b2b
CvdLans <93982741+CvdL-UM@users.noreply.github.com>
2024-10-08 14:21:16 +0200

    refactor: Use pnpm build and preview instead of pnpm dev web server

--- lawnotation-ui/playwright.config.ts
+++ lawnotation-ui/playwright.config.ts
@@ -103,10 +103,11 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'pnpm dev',
+    command: 'pnpm build && pnpm preview',
     // url: 'http://127.0.0.1:3000',
     // port: 3000,
     url: 'http://localhost:3000',
+    timeout: 10 * 60 * 1000,
     reuseExistingServer: !process.env.CI,
   },
 });
