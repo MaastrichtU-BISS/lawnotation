433f5e3a17724bb2314e6e84604ea210405fe87f
Parisxx <91888826+Parisxx@users.noreply.github.com>
2024-10-23 14:36:54 +0200

    refactor: Replace custom css with Tailwind classes

--- lawnotation-ui/components/ReportProblem.vue
+++ lawnotation-ui/components/ReportProblem.vue
@@ -46,7 +46,7 @@ const closePopup = () => {
         </template>
     </ConfirmPopup>
 
-    <Button @click="openPopup($event)" label="Report problem" outlined class="bg-white fixed z-50 w-24 h-10 text-xs font-light text-primary-500 border border-primary-500 hover:scale-110 hover:bg-white">
+    <Button @click="openPopup($event)" label="Report problem" outlined class="bg-white !fixed bottom-2 right-2 z-50 w-24 h-10 text-xs font-light text-primary-500 border border-primary-500 hover:scale-110 hover:bg-white">
         <template #icon>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
                 <path d="M3.5 2.75a.75.75 0 0 0-1.5 0v14.5a.75.75 0 0 0 1.5 0v-4.392l1.657-.348a6.449 6.449 0 0 1 4.271.572 7.948 7.948 0 0 0 5.965.524l2.078-.64A.75.75 0 0 0 18 12.25v-8.5a.75.75 0 0 0-.904-.734l-2.38.501a7.25 7.25 0 0 1-4.186-.363l-.502-.2a8.75 8.75 0 0 0-5.053-.439l-1.475.31V2.75Z" />
@@ -54,11 +54,3 @@ const closePopup = () => {
         </template>
     </Button>
 </template>
-
-<style scoped>
-.fixed {
-    position: fixed;
-    right: 10px;
-    bottom: 10px;
-}
-</style>
