293deb56b72cea3fc078b5c81b757571958b92f4
CvdLans <93982741+CvdL-UM@users.noreply.github.com>
2024-11-11 15:50:48 +0100

    style: Use PrimeVue to show warning message

--- lawnotation-ui/components/labels/Labelset.vue
+++ lawnotation-ui/components/labels/Labelset.vue
@@ -30,23 +30,12 @@
       ></textarea>
     </div>
     <hr class="my-3" />
-    <p v-if="numberOfTasksWithThisLabelset" class="flex gap-1 text-gray-700"
-      ><svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        class="size-6"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z"
-        />
-      </svg>
-      You can no longer add, remove and/or edit labels, since this labelset has
-      already been assigned to one or more task(s)</p
+    <Message
+      v-if="numberOfTasksWithThisLabelset"
+      severity="warn"
+      :closable="false"
+      >You can no longer add, remove and/or edit labels, since this labelset has
+      already been assigned to one or more task(s)</Message
     >
     <form v-else @submit.prevent="addLabel" class="flex space-x-4">
       <input v-model="newLabel.color" type="color" class="self-center base" />
