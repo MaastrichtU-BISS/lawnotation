e5206dc98a417cbce507fe7d04760fe41a816779
carloseam94 <carloseam94@gmail.com>
2025-03-06 14:58:56 +0100

    feat: add validateEmail function to utils

--- lawnotation-ui/utils/validators.ts
+++ lawnotation-ui/utils/validators.ts
@@ -1,3 +1,8 @@
 import z from "zod";
 
 export const zValidEmail = z.union([z.string().email(), z.string().regex(/@localhost$/)]);
+
+export function validateEmail(email: string) {
+    const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(email);
+}
