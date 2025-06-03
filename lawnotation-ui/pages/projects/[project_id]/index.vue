28d2fbb40a45b93c436ce52e84a2daa01c94db0a
Carlos <carloseam94@gmail.com>
2025-06-03 09:27:50 +0200

    fix: from document tyoe, omit id and hash

--- lawnotation-ui/pages/projects/[project_id]/index.vue
+++ lawnotation-ui/pages/projects/[project_id]/index.vue
@@ -515,7 +515,7 @@ const refreshLabelsets = async () => {
 
 const uploadDocuments = async (event: { files: FileList }) => {
 
-  const new_docs: Omit<Document, "id">[] = [];
+  const new_docs: Omit<Document, "id" | "hash">[] = [];
   upload_docs_progress.value.loading = true;
   upload_docs_progress.value.total = event.files.length ?? 0;
   upload_docs_progress.value.current = 0;
