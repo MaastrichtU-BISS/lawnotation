5dc46597f164ffd61676465d0502941945d835a5
CvdLans <93982741+CvdL-UM@users.noreply.github.com>
2025-07-30 15:21:18 +0200

    refactor: Different layout for Edit project page

--- lawnotation-ui/pages/projects/[project_id]/edit.vue
+++ lawnotation-ui/pages/projects/[project_id]/edit.vue
@@ -12,30 +12,42 @@
     <div v-if="project" class="dimmer-wrapper">
         <Dimmer v-model="loading" />
         <div class="dimmer-content">
-            <div class="flex justify-between">
-                <h3 class="mb-2 text-lg font-semibold text-center">Edit Project</h3>
-                <NuxtLink :to="`/projects/${project.id}`">
-                    <Button label="View" size="small" link />
-                </NuxtLink>
-            </div>
-            <div class="flex justify-center mb-4">
-                <span class="relative w-1/2">
-                    <InputText v-model="new_project.name" data-test="project-name" id="project_name" autocomplete="off"
-                        class="peer w-full block" placeholder="" />
-                    <label for="project_name"
-                        class="absolute text-sm text-primary-500 dark:text-primary-400/60 duration-300 transform -translate-y-4 scale-75 top-3 z-10 origin-[0] start-2.5 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">Name</label>
-                </span>
-            </div>
-            <div class="flex justify-center mb-4">
-                <div class="w-1/2">                
-                    <label for="project_description" class="block">Description</label>
-                    <Textarea v-model="new_project.desc" data-test="project-description" autoResize rows="5" cols="30"
-                        placeholder="Description" class="w-full" id="project_description" />
-                </div>
-            </div>
-        </div>
-        <div class="flex justify-center mb-5">
-            <Button label="Save Changes" @click="editProject" data-test="save-changes-button" />
+            <section class="grid md:grid-cols-2">
+                <form @submit.prevent="editProject">
+                    <Card>
+                        <template #title>
+                            <div class="flex items-center justify-between">
+                                <h3 class="text-lg font-semibold text-center">Edit Project</h3>
+                                <NuxtLink :to="`/projects/${project.id}`">
+                                    <Button label="View" size="small" link />
+                                </NuxtLink>
+                            </div>
+                        </template>
+                        <template #content>
+                            <main class="flex flex-col gap-4">
+                                <div class="flex flex-col relative">
+                                    <InputText v-model="new_project.name" required data-test="project-name"
+                                        id="project_name" autocomplete="off" class="peer w-full block" placeholder="" />
+                                    <label for="project_name"
+                                        class="absolute text-sm text-primary-500 dark:text-primary-400/60 duration-300 transform -translate-y-4 scale-75 top-3 z-10 origin-[0] start-2.5 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">Name<span
+                                            class="text-red-500">*</span></label>
+                                </div>
+                                <div class="flex flex-col">
+                                    <label for="project_description" class="block">Description</label>
+                                    <Textarea v-model="new_project.desc" data-test="project-description" autoResize
+                                        rows="5" cols="30" placeholder="Description" class="w-full"
+                                        id="project_description" />
+                                </div>
+                            </main>
+                        </template>
+                        <template #footer>
+                            <div class="flex justify-end">
+                                <Button type="submit" label="Save Changes" data-test="save-changes-button" />
+                            </div>
+                        </template>
+                    </Card>
+                </form>
+            </section>
         </div>
     </div>
 </template>
