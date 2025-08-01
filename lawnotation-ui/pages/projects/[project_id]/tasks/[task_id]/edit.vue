3f34a0441856458d1a4f83e2b613b2ea26751d9a
CvdLans <93982741+CvdL-UM@users.noreply.github.com>
2025-08-01 13:56:45 +0200

    refactor: Re-style Edit Task page and add danger zone to delete task

--- lawnotation-ui/pages/projects/[project_id]/tasks/[task_id]/edit.vue
+++ lawnotation-ui/pages/projects/[project_id]/tasks/[task_id]/edit.vue
@@ -20,39 +20,72 @@
     <div v-if="task" class="dimmer-wrapper pt-2">
         <Dimmer v-model="loading" />
         <div class="dimmer-content">
-            <div class="justify-center">
-                <div class="flex flex-col w-1/2 mx-auto mt-8 space-y-6 border-neutral-300">
-                    <h3 class="text-lg font-semibold">Edit Task</h3>
-                    <input class="base" type="text" placeholder="Task name" v-model="new_task.name" data-test="task-name" />
-                    <textarea class="base" placeholder="Task description" v-model="new_task.desc" data-test="task-description"></textarea>
-                    <textarea class="base" placeholder="Annotation Guidelines"
-                        v-model="new_task.ann_guidelines" data-test="annotation-guidelines"></textarea>
-                    <Dropdown :disabled="hasAssignments" v-model="new_task.labelset_id" :options="labelsets" optionLabel="name" optionValue="id" filter class="w-full text-left" />
-                    <Button @click="editTask" label="Save Changes" data-test="save-changes-button" />
-                </div>
-                <div v-if="new_emails && new_emails.length"
-                    class="flex flex-col my-5 w-1/2 mx-auto mt-8 space-y-2 border-neutral-300 pt-3">
-                    <h3 class="text-lg font-semibold">Replace Annotators</h3>
-                    <ul class="">
-                        <li v-for="(email, index) in new_emails" class="">
-                            <div class="relative mb-2">
-                                <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
-                                    <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
-                                        xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
-                                        <path
-                                            d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z" />
-                                        <path
-                                            d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z" />
-                                    </svg>
-                                </div>
-                                <input type="text" v-model="new_emails[index]"
-                                    :placeholder="`annotator ${annotators[index].annotator_number}`"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <div class="grid md:grid-cols-2 md:gap-6">
+                <Card class="flex flex-col h-fit">
+                    <template #title>
+                        <h3 class="text-lg font-semibold">Edit Task</h3>
+                    </template>
+                    <template #content>
+                        <main class="flex flex-col gap-4">
+                            <input class="base" type="text" placeholder="Task name" v-model="new_task.name"
+                                data-test="task-name" />
+                            <textarea class="base" placeholder="Task description" v-model="new_task.desc"
+                                data-test="task-description"></textarea>
+                            <textarea class="base" placeholder="Annotation Guidelines" v-model="new_task.ann_guidelines"
+                                data-test="annotation-guidelines"></textarea>
+                            <Dropdown :disabled="hasAssignments" v-model="new_task.labelset_id" :options="labelsets"
+                                optionLabel="name" optionValue="id" filter class="w-full text-left" />
+                        </main>
+                    </template>
+                    <template #footer>
+                        <div class="flex justify-end">
+                            <Button @click="editTask" label="Save Changes" data-test="save-changes-button" />
+                        </div>
+                    </template>
+                </Card>
+                <section class="flex flex-col gap-6">
+                    <Card v-if="new_emails && new_emails.length" class="flex flex-col gap-4 h-fit">
+                        <template #title>
+                            <h3 class="text-lg font-semibold">Replace Annotators</h3>
+                        </template>
+                        <template #content>
+                            <ul class="">
+                                <li v-for="(email, index) in new_emails" class="">
+                                    <div class="relative mb-2">
+                                        <div
+                                            class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                                            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                                                xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                                viewBox="0 0 20 16">
+                                                <path
+                                                    d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z" />
+                                                <path
+                                                    d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z" />
+                                            </svg>
+                                        </div>
+                                        <input type="text" v-model="new_emails[index]"
+                                            :placeholder="`annotator ${annotators[index].annotator_number}`"
+                                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                    </div>
+                                </li>
+                            </ul>
+                        </template>
+                        <template #footer>
+                            <div class="flex justify-end">
+                                <Button @click="replaceAnnotators" label="Replace" outlined />
                             </div>
-                        </li>
-                    </ul>
-                    <Button @click="replaceAnnotators" label="Replace" outlined />
-                </div>
+                        </template>
+                    </Card>
+                    <form>
+                        <Fieldset legend="Danger Zone" class="flex justify-center"
+                            :pt="{ root: { class: '!border-red-500' }, legend: { class: '!text-red-500 !bg-inherit border-none' } }"
+                            :ptOptions="{ mergeProps: true }">
+                            <Button label="Delete Task" severity="danger" icon="pi pi-trash" iconPos="right"
+                                @click="deleteTask(task)" data-test="delete-task-button" />
+                        </Fieldset>
+                    </form>
+                    <ConfirmBox />
+                </section>
             </div>
         </div>
     </div>
@@ -64,6 +97,7 @@ import Dimmer from "~/components/Dimmer.vue";
 import Dropdown from "primevue/dropdown";
 import Button from 'primevue/button';
 import { authorizeClient } from "~/utils/authorize.client";
+import { useConfirm } from "primevue/useconfirm";
 
 const { $toast, $trpc } = useNuxtApp();
 
@@ -102,6 +136,7 @@ const editTask = async () => {
         });
         loading.value = false;
         $toast.success("Task successfully edited!");
+        navigateTo(`/projects/${project.value?.id}`);
     } catch (error) {
         loading.value = false;
         $toast.error("Error editing Task");
@@ -159,9 +194,31 @@ const replaceAnnotators = async () => {
         if ((error as Object).hasOwnProperty('message')) {
             $toast.error((error as any).message)
         }
-    }    
+    }
 
     loading.value = false;
+};
+
+const confirm = useConfirm();
+const deleteTask = (task: Task) => {
+    confirm.require({
+        group: 'headless',
+        header: "Are you sure?",
+        message: `You are about to delete task ${task.name} and all annotations. This action cannot be undone.`,
+        accept: async () => {
+            try {
+                loading.value = true;
+                await $trpc.task.delete.mutate(task.id);
+                $toast.success("Task deleted successfully");
+                navigateTo(`/projects/${project.value!.id}`);
+            } catch (error) {
+                $toast.error(`Failed to delete task: ${error}`);
+            } finally {
+                loading.value = false;
+            }
+        },
+        reject: () => { }
+    });
 };
 
 onMounted(async () => {
